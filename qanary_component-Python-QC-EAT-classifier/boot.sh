818d6228ebab7ba54b64c8187f46a36272d30993
p-Heinze <heipa@posteo.de>
2021-11-07 12:59:36 +0100

    implement SSL support for Python QC-EAT-classifier

--- qanary_component-Python-QC-EAT-classifier/boot.sh
+++ qanary_component-Python-QC-EAT-classifier/boot.sh
@@ -4,5 +4,11 @@ echo The port number is: $SERVICE_PORT
 echo The Qanary pipeline URL is: $SPRING_BOOT_ADMIN_URL
 if [ -n $SERVICE_PORT ]
 then
-    exec gunicorn -b :$SERVICE_PORT --access-logfile - --error-logfile - run:app
+    if [ -f "$SERVER_SSL_CERTIFICATE" ] && [ -f "$SERVER_SSL_KEY" ]
+    then
+        echo Running with SSL configuration
+        exec gunicorn -b :$SERVICE_PORT --certfile $SERVER_SSL_CERTIFICATE --keyfile $SERVER_SSL_KEY --access-logfile - --error-logfile - run:app
+    else
+        exec gunicorn -b :$SERVICE_PORT --access-logfile - --error-logfile - run:app
+    fi
 fi
