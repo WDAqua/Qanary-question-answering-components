ab36f6f4be35cc53cd6765bbfe816e48accceeed
Calvin <dev@doernchen.eu>
2022-03-01 13:52:20 +0100

    add wikidata prefixes, remove unused imports

--- qanary_component-qb-platypus-wrapper/src/main/java/eu/wdaqua/qanary/platypus_wrapper/messages/PlatypusResult.java
+++ qanary_component-qb-platypus-wrapper/src/main/java/eu/wdaqua/qanary/platypus_wrapper/messages/PlatypusResult.java
@@ -2,14 +2,11 @@ package eu.wdaqua.qanary.platypus_wrapper.messages;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -40,8 +37,7 @@ public class PlatypusResult {
 
     public PlatypusResult(JSONObject json, String question, URI endpoint, String language) throws URISyntaxException {
         jsonParser = new JsonParser();
-        JsonObject parsedJsonObject = jsonParser.parse(json.toJSONString()).getAsJsonObject().getAsJsonObject("member");//.getAsJsonArray("member").getAsJsonArray();
-//        JsonArray parsedJsonArray = jsonParser.parse(json.toJSONString()).getAsJsonObject().getAsJsonArray("member").getAsJsonArray();
+        JsonObject parsedJsonObject = jsonParser.parse(json.toJSONString()).getAsJsonObject().getAsJsonObject("member");
 
         this.question = question;
         this.language = language;
@@ -67,8 +63,43 @@ public class PlatypusResult {
         logger.debug("0. confidence: {}", answers.get("resultScore").getAsDouble());
 
         this.confidence = answers.get("resultScore").getAsDouble();
-        this.sparql = answers.get("platypus:sparql").getAsString();
+        this.sparql = wikidataPrefixes() + answers.get("platypus:sparql").getAsString();
     }
+
+    private String wikidataPrefixes(){
+        String prefixes = "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                "PREFIX wds: <http://www.wikidata.org/entity/statement/>\n" +
+                "PREFIX wdv: <http://www.wikidata.org/value/>\n" +
+                "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+                "PREFIX wdref: <http://www.wikidata.org/reference/>\n" +
+                "PREFIX psv: <http://www.wikidata.org/prop/statement/value/>\n" +
+                "PREFIX psn: <http://www.wikidata.org/prop/statement/value-normalized/>\n" +
+                "PREFIX pqv: <http://www.wikidata.org/prop/qualifier/value/>\n" +
+                "PREFIX pqn: <http://www.wikidata.org/prop/qualifier/value-normalized/>\n" +
+                "PREFIX pr: <http://www.wikidata.org/prop/reference/>\n" +
+                "PREFIX prv: <http://www.wikidata.org/prop/reference/value/>\n" +
+                "PREFIX prn: <http://www.wikidata.org/prop/reference/value-normalized/>\n" +
+                "PREFIX wdno: <http://www.wikidata.org/prop/novalue/>\n" +
+                "PREFIX wdata: <http://www.wikidata.org/wiki/Special:EntityData/>\n" +
+                "PREFIX schema: <http://schema.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX prov: <http://www.w3.org/ns/prov#>\n" +
+                "PREFIX bds: <http://www.bigdata.com/rdf/search#>\n" +
+                "PREFIX gas: <http://www.bigdata.com/rdf/gas#>\n" +
+                "PREFIX hint: <http://www.bigdata.com/queryHints#>\n";
+
+        return prefixes;
+    }
+
 
     public JsonParser getJsonParser() {
         return jsonParser;
