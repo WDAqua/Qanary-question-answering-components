6e9339e5684d3358877fc3f68a2aae3a18accc00
Calvin <dev@doernchen.eu>
2022-03-15 16:48:20 +0100

    add TeBaQA component structure

---
+++ qanary_component-qb-tebaqa-wrapper/src/main/java/eu/wdaqua/qanary/tebaqa_wrapper/messages/TeBaQARequest.java
@@ -0,0 +1,92 @@
+package eu.wdaqua.qanary.tebaqa_wrapper.messages;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.constraints.NotBlank;
+import java.net.URI;
+
+public class TeBaQARequest {
+    private static final Logger logger = LoggerFactory.getLogger(TeBaQARequest.class);
+    @Schema(description = "Endpoint URL of TeBaQA API (default is already available)", example = "", required = false)
+    private URI tebaqaEndpointUrl;
+    @NotBlank
+    @Schema(description = "Question for that the results will be fetched from the gGnswer API", example = "What is the capital of Germany?", required = true)
+    private String question;
+    @Schema(description = "2-character language identifier (e.g., en, de, fr, it, es, pt)", example = "en", required = false)
+    private String language;
+
+    public TeBaQARequest() {
+
+    }
+
+    public TeBaQARequest(URI tebaqaEndpointUrl, String question, String language) {
+        this.tebaqaEndpointUrl = tebaqaEndpointUrl;
+        this.question = question;
+        this.language = language;
+    }
+
+    public TeBaQARequest(String question, String language) {
+        this.question = question;
+        this.language = language;
+    }
+
+    public TeBaQARequest(String question) {
+        this.question = question;
+    }
+
+    public URI getTeBaQAEndpointUrl() {
+        return tebaqaEndpointUrl;
+    }
+
+    public void setTeBaQAEndpointUrl(URI endpoint) {
+        this.tebaqaEndpointUrl = endpoint;
+    }
+
+    public String getTeBaQAQuestionUrlAsString() {
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+        parameters.add("question", getQuestion());
+
+        UriComponentsBuilder url = UriComponentsBuilder.fromUri(getTeBaQAEndpointUrl()).queryParams(parameters);
+        logger.info("request to {}", url.toUriString());
+
+        return url.toUriString();
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "TeBaQARequest: endpoint=" + this.getTeBaQAEndpointUrl() + ", question=" + this.getQuestion()
+                + ", lang=" + this.getLanguage();
+    }
+
+    public void replaceNullValuesWithDefaultValues(URI endpointDefault, String langDefault) {
+
+        if (this.getTeBaQAEndpointUrl() == null) {
+            this.setTeBaQAEndpointUrl(endpointDefault);
+        }
+
+        if (this.getLanguage() == null || this.getLanguage().isBlank()) {
+            this.setLanguage(langDefault);
+        }
+    }
+}
---
+++ qanary_component-qb-tebaqa-wrapper/src/main/java/eu/wdaqua/qanary/tebaqa_wrapper/messages/TeBaQAResult.java
@@ -0,0 +1,103 @@
+package eu.wdaqua.qanary.tebaqa_wrapper.messages;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import net.minidev.json.JSONObject;
+
+public class TeBaQAResult {
+    private static final Logger logger = LoggerFactory.getLogger(TeBaQAResult.class);
+
+    private com.google.gson.JsonParser jsonParser;
+
+    private URI endpoint;
+    private String language;
+    private String question;
+
+    private String sparql;
+    private List<String> values;
+    private String type;
+    private URI datatype;
+    private double confidence;
+
+    @Hidden
+    public final URI RESOURCETYPEURI;
+    @Hidden
+    public final URI BOOLEANTYPEURI;
+    @Hidden
+    public final URI STRINGTYPEURI;
+
+    public TeBaQAResult(JSONObject json, String question, URI endpoint, String language) throws URISyntaxException {
+        jsonParser = new JsonParser();
+        JsonArray parsedJsonArray = jsonParser.parse(json.toJSONString()).getAsJsonObject().getAsJsonArray("queries").getAsJsonArray();
+
+        this.question = question;
+        this.language = language;
+        this.endpoint = endpoint;
+
+        this.RESOURCETYPEURI = new URI("http://www.w3.org/2001/XMLSchema#anyURI");
+        this.BOOLEANTYPEURI = new URI("http://www.w3.org/2001/XMLSchema#boolean");
+        this.STRINGTYPEURI = new URI("http://www.w3.org/2001/XMLSchema#string");
+
+        initData(parsedJsonArray);
+    }
+
+    /**
+     * init the fields while parsing the JSON data
+     *
+     * @param answers
+     * @throws URISyntaxException
+     */
+    private void initData(JsonArray answers) throws URISyntaxException {
+        logger.debug("responseQuestion: {}", answers);
+        // TODO init data
+
+    }
+
+    public JsonParser getJsonParser() {
+        return jsonParser;
+    }
+
+    public URI getEndpoint() {
+        return endpoint;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getSparql() {
+        return sparql;
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public URI getDatatype() {
+        return datatype;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+}
