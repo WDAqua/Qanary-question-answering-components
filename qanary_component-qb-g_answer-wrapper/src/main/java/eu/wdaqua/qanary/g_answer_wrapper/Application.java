4a8a6ebb3c4e46ca6efe6ffc9a19e366813a212a
Calvin <dev@doernchen.eu>
2022-03-28 19:39:13 +0200

    add sampele openAPI to gAnswer component

--- qanary_component-qb-g_answer-wrapper/src/main/java/eu/wdaqua/qanary/g_answer_wrapper/Application.java
+++ qanary_component-qb-g_answer-wrapper/src/main/java/eu/wdaqua/qanary/g_answer_wrapper/Application.java
@@ -1,5 +1,9 @@
 package eu.wdaqua.qanary.g_answer_wrapper;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,8 +28,7 @@ import java.util.ArrayList;
 /**
  * basic class for wrapping functionality to a Qanary component
  * note: there is no need to change something here
- */
-public class Application {
+ */ public class Application {
 
     /**
      * this method is needed to make the QanaryComponent in this project known
@@ -71,19 +74,15 @@ public class Application {
         return new GAnswerQueryBuilder(threshold, langDefault, supportedLang, endpoint, applicationName, restTemplate);
     }
 
-	/*
-	@Bean
-	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
-		return new OpenAPI().info(new Info() //
-				.title("SimpleSpringService Data API") //
-				.version(appVersion) //
-				.description("This is a sample Foobar server created using springdocs - "
-						+ "a library for OpenAPI 3 with spring boot.")
-				.termsOfService("http://swagger.io/terms/") //
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")) //
-		);
-	}
-	*/
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI().info(new Info() //
+                .title("SimpleSpringService Data API") //
+                .version(appVersion) //
+                .description("This is a sample Foobar server created using springdocs - " + "a library for OpenAPI 3 with spring boot.").termsOfService("http://swagger.io/terms/") //
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")) //
+        );
+    }
 
     @Autowired
     public QanaryComponentConfiguration qanaryComponentConfiguration;
