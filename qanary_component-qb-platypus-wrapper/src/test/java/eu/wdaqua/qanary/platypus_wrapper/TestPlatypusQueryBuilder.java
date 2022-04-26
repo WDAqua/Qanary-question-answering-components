fde313e1fd346c907c87252f94ec407b57867364
Calvin <dev@doernchen.eu>
2022-04-26 13:20:31 +0200

    add caching test for the component

--- qanary_component-qb-platypus-wrapper/src/test/java/eu/wdaqua/qanary/platypus_wrapper/TestPlatypusQueryBuilder.java
+++ qanary_component-qb-platypus-wrapper/src/test/java/eu/wdaqua/qanary/platypus_wrapper/TestPlatypusQueryBuilder.java
@@ -1,5 +1,7 @@
 package eu.wdaqua.qanary.platypus_wrapper;
 
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
+import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
 import eu.wdaqua.qanary.platypus_wrapper.messages.PlatypusResult;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,15 +13,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -27,13 +33,18 @@ import static org.junit.Assert.assertTrue;
 public class TestPlatypusQueryBuilder {
     private static final Logger logger = LoggerFactory.getLogger(TestPlatypusQueryBuilder.class);
     private final String applicationName = "PlatypusQueryBuilder";
+    // time span for caching, tests wait this time span during the test runs
+    protected final static int MAX_TIME_SPAN_SECONDS = 5;
+    private URI endpoint;
+
+    private enum Cache {CACHED, NOT_CACHED}
 
     @Autowired
     private Environment env;
-
-    URI endpoint;
-    RestTemplate restTemplate;
-
+    @Autowired
+    private RestTemplateWithCaching restTemplate;
+    @Autowired
+    private CacheOfRestTemplateResponse myCacheOfResponse;
 
     @Before
     public void init() throws URISyntaxException {
@@ -41,7 +52,6 @@ public class TestPlatypusQueryBuilder {
         this.endpoint = new URI(env.getProperty("platypus.endpoint.url"));
         assert (this.endpoint != null) : "platypus.endpoint.url cannot be empty";
 
-        this.restTemplate = new RestTemplate();
         assert this.restTemplate != null : "restTemplate cannot be null";
     }
 
@@ -56,7 +66,7 @@ public class TestPlatypusQueryBuilder {
         String langDefault = "en";
         ArrayList<String> supportedLang = new ArrayList<String>(Arrays.asList("en", "fr", "es"));
 
-        PlatypusQueryBuilder platypusQueryBuilder = new PlatypusQueryBuilder(threshold, langDefault, supportedLang, this.endpoint, this.applicationName, this.restTemplate);
+        PlatypusQueryBuilder platypusQueryBuilder = new PlatypusQueryBuilder(threshold, langDefault, supportedLang, this.endpoint, this.applicationName, this.restTemplate, this.myCacheOfResponse);
 
         assertTrue(platypusQueryBuilder.isLangSuppoerted("en"));
         assertTrue(platypusQueryBuilder.isLangSuppoerted("fr"));
@@ -76,7 +86,7 @@ public class TestPlatypusQueryBuilder {
         String langDefault = "en";
         ArrayList<String> supportedLang = new ArrayList<String>(Arrays.asList("en", "fr", "es"));
 
-        PlatypusQueryBuilder platypusQueryBuilder = new PlatypusQueryBuilder(threshold, langDefault, supportedLang, this.endpoint, this.applicationName, this.restTemplate);
+        PlatypusQueryBuilder platypusQueryBuilder = new PlatypusQueryBuilder(threshold, langDefault, supportedLang, this.endpoint, this.applicationName, this.restTemplate, this.myCacheOfResponse);
 
         String question = "population of france";
         PlatypusResult result0 = testWebService(platypusQueryBuilder, question, langDefault);
@@ -123,4 +133,89 @@ public class TestPlatypusQueryBuilder {
         assertFalse(result.getSparql().isEmpty());
         return result;
     }
+
+    @Test
+    public void givenRestTemplate_whenRequested_thenLogAndModifyResponse() throws InterruptedException, URISyntaxException {
+
+        assertNotNull(restTemplate);
+        assertNotNull(myCacheOfResponse);
+
+        LoginForm loginForm0 = new LoginForm("userName", "password");
+        LoginForm loginForm1 = new LoginForm("userName2", "password2");
+
+        assertEquals(0, myCacheOfResponse.getNumberOfExecutedRequests());
+
+        callRestTemplateWithCaching(loginForm0, Cache.NOT_CACHED); // cache miss
+        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
+        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
+        TimeUnit.SECONDS.sleep(MAX_TIME_SPAN_SECONDS + 1); // wait until it is too late for caching
+        callRestTemplateWithCaching(loginForm0, Cache.NOT_CACHED); // cache miss: too long ago
+        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
+        callRestTemplateWithCaching(loginForm1, Cache.NOT_CACHED); // cache miss: different body
+        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
+        callRestTemplateWithCaching(loginForm1, Cache.CACHED); // cache hit
+
+        assertEquals(3, myCacheOfResponse.getNumberOfExecutedRequests());
+
+    }
+
+    private void callRestTemplateWithCaching(LoginForm loginForm, Cache cacheStatus) throws URISyntaxException {
+        URI TESTSERVICEURL = new URI("http://httpbin.org/post");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<LoginForm> requestEntity = new HttpEntity<LoginForm>(loginForm, headers);
+
+        long numberOfNewlyExecutedRequests = myCacheOfResponse.getNumberOfExecutedRequests();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(TESTSERVICEURL, requestEntity, String.class);
+        numberOfNewlyExecutedRequests = myCacheOfResponse.getNumberOfExecutedRequests()
+                - numberOfNewlyExecutedRequests;
+        logger.info("numberOfExecutedRequest since last request: new={}, count={}, teststatus={}", //
+                numberOfNewlyExecutedRequests, myCacheOfResponse.getNumberOfExecutedRequests(), cacheStatus);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        switch (cacheStatus) {
+            case NOT_CACHED:
+                assertEquals(1, numberOfNewlyExecutedRequests);
+                break;
+            case CACHED:
+                assertEquals(0, numberOfNewlyExecutedRequests);
+                break;
+            default:
+                fail("Test case misconfigured");
+                break;
+        }
+    }
+
+    public class LoginForm {
+        private String username;
+        private String password;
+
+        public LoginForm() {
+        }
+
+        public LoginForm(String username, String password) {
+            super();
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
 }
