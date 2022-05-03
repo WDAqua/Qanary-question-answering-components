ae15a7bd4fb24b3e09d866c46dd764ad08cd9a59
Calvin <dev@doernchen.eu>
2022-05-03 12:00:45 +0200

    remove old config

--- qanary_component-qb-platypus-wrapper/src/test/java/eu/wdaqua/qanary/platypus_wrapper/RestTemplateCacheLiveTestConfiguration.java
+++ qanary_component-qb-platypus-wrapper/src/test/java/eu/wdaqua/qanary/platypus_wrapper/RestTemplateCacheLiveTestConfiguration.java
@@ -7,7 +7,6 @@ public class RestTemplateCacheLiveTestConfiguration {
     // define here the current CaffeineCacheManager configuration
     static {
         System.setProperty("platypus.endpoint.url", "http://some-platypus-endpoint-url.com/endpoint");
-//        System.setProperty("platypus.endpoint.url", "http://127.0.0.1:41021/platypus/answer_raw");
         System.setProperty("qanary.webservicecalls.cache.specs",
                 "maximumSize=1000,expireAfterAccess=" + TestPlatypusQueryBuilder.MAX_TIME_SPAN_SECONDS + "s");
     }
