63719d34c5b4dc79d71f47b09933ecda9eb5201a
anbo-de <github@andreasboth.de>
2022-05-04 03:26:49 +0200

    fixed init problem and bug in SPARQL query

--- qanary_component-NER-DBpedia-Spotlight/src/main/java/eu/wdaqua/qanary/dbpediaSpotlight/Application.java
+++ qanary_component-NER-DBpedia-Spotlight/src/main/java/eu/wdaqua/qanary/dbpediaSpotlight/Application.java
@@ -2,6 +2,8 @@ package eu.wdaqua.qanary.dbpediaSpotlight;
 
 import com.google.gson.JsonArray;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -19,46 +21,53 @@ import eu.wdaqua.qanary.exceptions.DBpediaSpotlightServiceNotAvailable;
 @EnableAutoConfiguration
 @ComponentScan("eu.wdaqua.qanary.component")
 public class Application {
-    /**
-     * this method is needed to make the QanaryComponent in this project known to the
-     * QanaryServiceController in the qanary_component-template
-     */
-    @Bean
-    public QanaryComponent qanaryComponent(@Value("${spring.application.name}") final String applicationName) {
-        return new DBpediaSpotlightNER(applicationName);
-    }
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @Bean
-    public DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration(
-            @Value("${dbpediaspotlight.test-question}") String testQuestion,
-            @Value("${dbpediaspotlight.confidence.minimum}") float confidenceMinimum,
-            @Value("${dbpediaspotlight.endpoint:https://api.dbpedia-spotlight.org/en/annotate}") String endpoint,
-            DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher
-    ) throws DBpediaSpotlightServiceNotAvailable {
-        this.checkSpotlightServiceAvailability(
-                testQuestion, endpoint, confidenceMinimum, dBpediaSpotlightServiceFetcher);
-        return new DBpediaSpotlightConfiguration(confidenceMinimum, endpoint);
-    }
+	/**
+	 * this method is needed to make the QanaryComponent in this project known to
+	 * the QanaryServiceController in the qanary_component-template
+	 */
+	@Bean
+	public QanaryComponent qanaryComponent( //
+			@Value("${spring.application.name}") final String applicationName, //
+			DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration, //
+			DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher //
+	) {
+		return new DBpediaSpotlightNER(applicationName, dBpediaSpotlightConfiguration, dBpediaSpotlightServiceFetcher);
+	}
 
-    @Bean DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher() {
-        return new DBpediaSpotlightServiceFetcher();
-    }
+	@Bean
+	public DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration( // 
+			@Value("${dbpediaspotlight.test-question}") String testQuestion, // 
+			@Value("${dbpediaspotlight.confidence.minimum}") float confidenceMinimum, // 
+			@Value("${dbpediaspotlight.endpoint:https://api.dbpedia-spotlight.org/en/annotate}") String endpoint, // 
+			DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher //
+	) throws DBpediaSpotlightServiceNotAvailable {
+		this.checkSpotlightServiceAvailability(testQuestion, endpoint, confidenceMinimum,
+				dBpediaSpotlightServiceFetcher);
+		logger.debug("endpoint: {}", endpoint);
+		return new DBpediaSpotlightConfiguration(confidenceMinimum, endpoint);
+	}
 
-    private void checkSpotlightServiceAvailability(
-            String testQuestion, String endpoint, float confidenceMinimum,
-            DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher) throws DBpediaSpotlightServiceNotAvailable {
-        String err;
-        try {
-            JsonArray response = dBpediaSpotlightServiceFetcher.getJsonFromService(
-                    testQuestion, endpoint, confidenceMinimum);
-            return;
-        } catch (Exception e) {
-            err = e.getLocalizedMessage();
-        }
-        throw new DBpediaSpotlightServiceNotAvailable("No response from endpoint "+endpoint+"!\n"+err);
-    }
+	@Bean
+	DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher() {
+		return new DBpediaSpotlightServiceFetcher();
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	private void checkSpotlightServiceAvailability(String testQuestion, String endpoint, float confidenceMinimum,
+			DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher) throws DBpediaSpotlightServiceNotAvailable {
+		String err;
+		try {
+			JsonArray response = dBpediaSpotlightServiceFetcher.getJsonFromService(testQuestion, endpoint,
+					confidenceMinimum);
+			return;
+		} catch (Exception e) {
+			err = e.getLocalizedMessage();
+		}
+		throw new DBpediaSpotlightServiceNotAvailable("No response from endpoint " + endpoint + "!\n" + err);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
--- qanary_component-NER-DBpedia-Spotlight/src/main/java/eu/wdaqua/qanary/dbpediaSpotlight/DBpediaSpotlightNER.java
+++ qanary_component-NER-DBpedia-Spotlight/src/main/java/eu/wdaqua/qanary/dbpediaSpotlight/DBpediaSpotlightNER.java
@@ -8,6 +8,7 @@ import com.google.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import eu.wdaqua.qanary.commons.QanaryMessage;
 import eu.wdaqua.qanary.commons.QanaryQuestion;
 import eu.wdaqua.qanary.commons.QanaryUtils;
 import eu.wdaqua.qanary.component.QanaryComponent;
+import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
 
 /**
  * represents a wrapper of the DBpedia Spotlight tool used here as a spotter
@@ -23,111 +25,107 @@ import eu.wdaqua.qanary.component.QanaryComponent;
 
 @Component
 public class DBpediaSpotlightNER extends QanaryComponent {
-    private static final Logger logger = LoggerFactory.getLogger(DBpediaSpotlightNER.class);
+	private static final Logger logger = LoggerFactory.getLogger(DBpediaSpotlightNER.class);
 
-    private final String applicationName;
+	private final String applicationName;
+	private final DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration;
+	private final DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher;
 
-    @Inject
-	private DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration;
+	public DBpediaSpotlightNER( //
+			final String applicationName, //
+			DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration, //
+			DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher //
+	) {
+		this.applicationName = applicationName;
+		this.dBpediaSpotlightConfiguration = dBpediaSpotlightConfiguration;
+		this.dBpediaSpotlightServiceFetcher = dBpediaSpotlightServiceFetcher;
+		logger.debug("endpoint: {}", this.dBpediaSpotlightConfiguration.getEndpoint());
+	}
 
-	@Inject
-	private DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher;
+	/**
+	 * default processor of a QanaryMessage
+	 * 
+	 * @throws Exception
+	 */
+	public QanaryMessage process(QanaryMessage myQanaryMessage) throws Exception {
+		long startTime = System.currentTimeMillis();
+		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
+		logger.info("Qanary Message: {}", myQanaryMessage);
 
+		// STEP1: Retrieve the named graph and the endpoint
 
-    public DBpediaSpotlightNER(@Value("${spring.application.name}") final String applicationName) {
-        this.applicationName = applicationName;
-    }
+		// the class QanaryUtils provides some helpers for standard tasks
+		QanaryUtils myQanaryUtils = this.getUtils(myQanaryMessage);
+		QanaryQuestion<String> myQanaryQuestion = this.getQanaryQuestion(myQanaryMessage);
 
-    /**
-     * default processor of a QanaryMessage
-     */
-    public QanaryMessage process(QanaryMessage myQanaryMessage) {
-        long startTime = System.currentTimeMillis();
-        org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
-        logger.info("Qanary Message: {}", myQanaryMessage);
+		// question string is required as input for the service call
+		String myQuestion = myQanaryQuestion.getTextualRepresentation();
+		logger.info("process question \"{}\" with DBpedia Spotlight at {} and minimum confidence: {}", myQuestion,
+				dBpediaSpotlightConfiguration.getEndpoint(), dBpediaSpotlightConfiguration.getConfidenceMinimum());
 
-        try {
+		// STEP2: Call the DBpedia service
+		JsonArray resources;
+		resources = dBpediaSpotlightServiceFetcher.getJsonFromService(myQuestion, //
+				dBpediaSpotlightConfiguration.getEndpoint(), //
+				dBpediaSpotlightConfiguration.getConfidenceMinimum() //
+		);
 
-            // STEP1: Retrieve the named graph and the endpoint
+		// get all found DBpedia entities
+		List<FoundDBpediaResource> foundDBpediaResources = new LinkedList<>();
+		for (int i = 0; i < resources.size(); i++) {
+			foundDBpediaResources.add(new FoundDBpediaResource(resources.get(i)));
+			logger.debug("found entity ({} of {}): at ({},{})", //
+					i, resources.size() - 1, //
+					foundDBpediaResources.get(i).getBegin(), //
+					foundDBpediaResources.get(i).getEnd() //
+			);
+		}
 
-            // the class QanaryUtils provides some helpers for standard tasks
-            QanaryUtils myQanaryUtils = this.getUtils(myQanaryMessage);
-            QanaryQuestion<String> myQanaryQuestion = this.getQanaryQuestion(myQanaryMessage);
+		// STEP3: Pass the information to the component and execute it
 
-            // question string is required as input for the service call
-            String myQuestion = myQanaryQuestion.getTextualRepresentation();
-            logger.info("process question \"{}\" with DBpedia Spotlight at {} and minimum confidence: {}", 
-                    myQuestion, dBpediaSpotlightConfiguration.getEndpoint(),
-				    dBpediaSpotlightConfiguration.getConfidenceMinimum());
-            	
-            // STEP2: Call the DBpedia service
-            JsonArray resources;
-            resources = dBpediaSpotlightServiceFetcher.getJsonFromService(
-                    myQuestion, //
-                    dBpediaSpotlightConfiguration.getEndpoint(), //
-                    dBpediaSpotlightConfiguration.getConfidenceMinimum() //
-            );
+		// create one larger SPARQL INSERT query that adds all discovered named entities
+		// at once
+		String sparql, sparqlbind;
+		sparql = "" //
+				+ "PREFIX qa: <http://www.wdaqua.eu/qa#> " //
+				+ "PREFIX oa: <http://www.w3.org/ns/openannotation/core/> " //
+				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " //
+				+ "INSERT { ";
+		sparqlbind = "";
+		int i = 0;
 
-            // get all found DBpedia entities
-            List<FoundDBpediaResource> foundDBpediaResources = new LinkedList<>();
-            for (int i = 0; i < resources.size(); i++) {
-                foundDBpediaResources.add(new FoundDBpediaResource(resources.get(i)));
-                logger.debug("found entity ({} of {}): at ({},{})", //
-                        i, resources.size() - 1, //
-                        foundDBpediaResources.get(i).getBegin(), //
-                        foundDBpediaResources.get(i).getEnd() //
-                );
-            }
+		for (FoundDBpediaResource found : foundDBpediaResources) {
+			sparql += "" //
+					+ "GRAPH <" + myQanaryQuestion.getOutGraph() + "> { " //
+					+ "  ?a" + i + " a qa:AnnotationOfSpotInstance . " //
+					+ "  ?a" + i + " oa:hasTarget [ " //
+					+ "           a    oa:SpecificResource; " //
+					+ "           oa:hasSource    <" + myQanaryQuestion.getUri() + ">; " //
+					+ "           oa:hasSelector  [ " //
+					+ "                    a oa:TextPositionSelector ; " //
+					+ "                    oa:start \"" + found.getBegin() + "\"^^xsd:nonNegativeInteger ; " //
+					+ "                    oa:end  \"" + found.getEnd() + "\"^^xsd:nonNegativeInteger  " //
+					+ "           ] " //
+					+ "  ] ; " //
+					+ "     oa:annotatedBy <urn:qanary:" + this.applicationName + "> ; " //
+					+ "	    oa:annotatedAt ?time  " //
+					+ "} "; // end: graph
+			sparqlbind += "  BIND (IRI(str(RAND())) AS ?a" + i + ") .";
+			i++;
+		}
 
-            // STEP3: Pass the information to the component and execute it
+		sparql += "" //
+				+ "} " // end: insert
+				+ "WHERE { " //
+				+ sparqlbind //
+				+ "  BIND (now() as ?time) " //
+				+ "}";
 
-            // create one larger SPARQL INSERT query that adds all discovered named entities
-            // at once
-            String sparql, sparqlbind;
-            sparql = "" //
-                    + "PREFIX qa: <http://www.wdaqua.eu/qa#> " //
-                    + "PREFIX oa: <http://www.w3.org/ns/openannotation/core/> " //
-                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " //
-                    + "INSERT { ";
-            sparqlbind = "";
-            int i = 0;
+		myQanaryUtils.updateTripleStore(sparql, myQanaryQuestion.getEndpoint().toString());
 
-            for (FoundDBpediaResource found : foundDBpediaResources) {
-                sparql = "" //
-                        + "GRAPH <" + myQanaryQuestion.getOutGraph() + "> { " //
-                        + "  ?a"+i+" a qa:AnnotationOfSpotInstance . " //
-                        + "  ?a"+i+" oa:hasTarget [ " //
-                        + "           a    oa:SpecificResource; " //
-                        + "           oa:hasSource    <" + myQanaryQuestion.getUri() + ">; " //
-                        + "           oa:hasSelector  [ " //
-                        + "                    a oa:TextPositionSelector ; " //
-                        + "                    oa:start \"" + found.getBegin() + "\"^^xsd:nonNegativeInteger ; " //
-                        + "                    oa:end  \"" + found.getEnd() + "\"^^xsd:nonNegativeInteger  " //
-                        + "           ] " //
-                        + "  ] ; " //
-                        + "     oa:annotatedBy <urn:qanary:"+this.applicationName+"> ; " //
-                        + "	    oa:annotatedAt ?time  " //
-                        + "} "; // end: graph
-                sparqlbind += "  BIND (IRI(str(RAND())) AS ?a" + i + ") .";
-                i++;
-            }
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		logger.info("Time {}", estimatedTime);
 
-            sparql += "" //
-                    + "} " // end: insert
-                    + "WHERE { " //
-                    + sparqlbind //
-                    + "  BIND (now() as ?time) " //
-                    + "}";
-
-            myQanaryUtils.updateTripleStore(sparql, myQanaryQuestion.getEndpoint().toString());
-
-            long estimatedTime = System.currentTimeMillis() - startTime;
-            logger.info("Time {}", estimatedTime);
-
-        } catch (Exception e) {// MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        return myQanaryMessage;
-    }
+		return myQanaryMessage;
+	}
 }
