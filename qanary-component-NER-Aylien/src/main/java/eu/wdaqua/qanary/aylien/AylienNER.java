08c7641f2deb34c90e02287945d03105b153b34e
Calvin <dev@doernchen.eu>
2022-05-31 10:52:22 +0200

    update component NER Aylien for qanary 3.1.2 (https://github.com/anbo-de/team-tasks/issues/60)

--- qanary-component-NER-Aylien/src/main/java/eu/wdaqua/qanary/aylien/AylienNER.java
+++ qanary-component-NER-Aylien/src/main/java/eu/wdaqua/qanary/aylien/AylienNER.java
@@ -1,14 +1,9 @@
 package eu.wdaqua.qanary.aylien;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Map;
-
+import eu.wdaqua.qanary.commons.QanaryMessage;
+import eu.wdaqua.qanary.commons.QanaryQuestion;
+import eu.wdaqua.qanary.commons.QanaryUtils;
+import eu.wdaqua.qanary.component.QanaryComponent;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -23,10 +18,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import eu.wdaqua.qanary.commons.QanaryMessage;
-import eu.wdaqua.qanary.commons.QanaryQuestion;
-import eu.wdaqua.qanary.commons.QanaryUtils;
-import eu.wdaqua.qanary.component.QanaryComponent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 
 @Component
@@ -53,8 +49,8 @@ public class AylienNER extends QanaryComponent {
 		logger.info("process: {}", myQanaryMessage);
 		// TODO: implement processing of question
 		QanaryUtils myQanaryUtils = this.getUtils(myQanaryMessage);
-	      QanaryQuestion<String> myQanaryQuestion = new QanaryQuestion(myQanaryMessage);
-	      String myQuestion = myQanaryQuestion.getTextualRepresentation();
+		QanaryQuestion<String> myQanaryQuestion = new QanaryQuestion(myQanaryMessage, myQanaryUtils.getQanaryTripleStoreConnector());
+		String myQuestion = myQanaryQuestion.getTextualRepresentation();
 	      ArrayList<Selection> selections = new ArrayList<Selection>();
 	      //ArrayList<Link> links = new ArrayList<Link>();
 	      logger.info("Question {}", myQuestion);
