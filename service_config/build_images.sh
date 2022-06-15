561c3a79d44b311ffedae276b3b274f84748eac0
Oliver Schmidtke <oliver.schmidtke@student.hs-anhalt.de>
2022-06-15 15:16:52 +0200

    Updated build process
    The docker image build process has been extended with the necessary build
    of the Qanary pipeline.

--- service_config/build_images.sh
+++ service_config/build_images.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# clone Qanary pipeline
+git clone https://github.com/WDAqua/Qanary.git
+
+# subshell building the Qanary pipeline
+(
+cd Qanary/
+mvn clean install -Ddockerfile.skip=true -DskipTests
+)
+
+# delete Qanary pipeline repository
+rm -rf Qanary/
+
 # build Docker Images and store name and tag
 mvn clean package -DskipTests
-docker image ls | grep -oP "qanary.*\.[0-9] " > images.temp
+docker image ls | grep -oP "^qanary/qanary-component.*\.[0-9] " > images.temp
 
 # read image list
 images=$(cat images.temp)
@@ -26,10 +38,9 @@ do
     i=0
 
     # tag images and push to Dockerhub
-    docker tag "${file_name}" "qanary/${file_name}"
-    docker tag "${file_name}" "qanary/${latest_file_name}"
-    docker push "qanary/${file_name}"
-    docker push "qanary/${latest_file_name}"
+    docker tag "${file_name}" "${latest_file_name}"
+    docker push "${file_name}"
+    docker push "${latest_file_name}"
   fi
 done
 
