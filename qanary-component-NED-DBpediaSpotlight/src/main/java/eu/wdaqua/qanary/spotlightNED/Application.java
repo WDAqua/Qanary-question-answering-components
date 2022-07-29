8a2f056a909baa0d644df79c6b07cf06c4058b98
Calvin <dev@doernchen.eu>
2022-07-29 14:00:08 +0200

    add RestTemplateWithCaching, add tests

--- qanary-component-NED-DBpediaSpotlight/src/main/java/eu/wdaqua/qanary/spotlightNED/Application.java
+++ qanary-component-NED-DBpediaSpotlight/src/main/java/eu/wdaqua/qanary/spotlightNED/Application.java
@@ -1,63 +1,75 @@
 package eu.wdaqua.qanary.spotlightNED;
 
 import com.google.gson.JsonArray;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
 import eu.wdaqua.qanary.component.QanaryComponent;
 import eu.wdaqua.qanary.exceptions.DBpediaSpotlightServiceNotAvailable;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableCaching
-@ComponentScan("eu.wdaqua.qanary.component")
+@ComponentScan(basePackages = {"eu.wdaqua.qanary"})
 public class Application {
-	
-	@Bean
-	public DBpediaSpotlightConfiguration myDBpediaSpotlightConfiguration( //
-            @Value("${dbpediaspotlight.test-question}") String testQuestion, //
-			@Value("${dbpediaspotlight.confidence.minimum}") float confidenceMinimum, //
-			@Value("${dbpediaspotlight.endpoint:https://api.dbpedia-spotlight.org/en/annotate}") String endpoint //
-	) throws DBpediaSpotlightServiceNotAvailable {
-        this.checkSpotlightServiceAvailability(
-                testQuestion, endpoint, confidenceMinimum, myDBpediaSpotlightServiceFetcher());
-		return new DBpediaSpotlightConfiguration(confidenceMinimum, endpoint);
-	}
+    private static ApplicationContext applicationContext;
+    @Autowired
+    CacheOfRestTemplateResponse myCacheOfResponses;
+    @Autowired
+    RestTemplate restTemplate;
 
-	@Bean
-	public QanaryComponent qanaryComponent() {
-		return new DBpediaSpotlightNED();
-	}
-	
-	@Bean
-	public DBpediaSpotlightServiceFetcher myDBpediaSpotlightServiceFetcher() {
-		return new DBpediaSpotlightServiceFetcher();
-	}
+    /**
+     * default main
+     */
+    public static void main(String[] args) {
+        applicationContext = SpringApplication.run(Application.class, args);
+    }
 
-    private void checkSpotlightServiceAvailability(
-            String testQuestion, String endpoint, float confidenceMinimum,
-            DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher) throws DBpediaSpotlightServiceNotAvailable {
+    @Bean
+    public DBpediaSpotlightConfiguration myDBpediaSpotlightConfiguration(//
+                                                                         @Value("${dbpediaspotlight.test-question}") String testQuestion, //
+                                                                         @Value("${dbpediaspotlight.confidence.minimum}") float confidenceMinimum, //
+                                                                         @Value("${dbpediaspotlight.endpoint:https://api.dbpedia-spotlight.org/en/annotate}") String endpoint //
+    ) throws DBpediaSpotlightServiceNotAvailable {
+        this.checkSpotlightServiceAvailability(testQuestion, endpoint, confidenceMinimum, myDBpediaSpotlightServiceFetcher());
+        return new DBpediaSpotlightConfiguration(confidenceMinimum, endpoint);
+    }
+
+    @Bean
+    public QanaryComponent qanaryComponent() {
+        return new DBpediaSpotlightNED();
+    }
+
+    @Bean
+    public DBpediaSpotlightServiceFetcher myDBpediaSpotlightServiceFetcher() {
+        return new DBpediaSpotlightServiceFetcher();
+    }
+
+    private void checkSpotlightServiceAvailability(String testQuestion, String endpoint, float confidenceMinimum, DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher) throws DBpediaSpotlightServiceNotAvailable {
         String err;
         try {
-            JsonArray response = dBpediaSpotlightServiceFetcher.getJsonFromService(
-                    testQuestion, endpoint, confidenceMinimum);
+            JsonArray response = dBpediaSpotlightServiceFetcher.getJsonFromService(testQuestion, endpoint, confidenceMinimum, restTemplate, myCacheOfResponses);
             return;
         } catch (Exception e) {
             err = e.getLocalizedMessage();
         }
-        throw new DBpediaSpotlightServiceNotAvailable("No response from endpoint "+endpoint+"!\n"+err);
+        throw new DBpediaSpotlightServiceNotAvailable("No response from endpoint " + endpoint + "!\n" + err);
     }
 
-	/**
-	 * default main
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI().info(new Info() //
+                .title("NED DBpediaSpotlight component") //
+                .version(appVersion) //
+                .description("This is a sample Foobar server created using springdocs - " + "a library for OpenAPI 3 with spring boot.").termsOfService("http://swagger.io/terms/") //
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")) //
+        );
+    }
 }
--- qanary-component-NED-DBpediaSpotlight/src/test/java/eu/wdaqua/qanary/spotlightNED/TestQanaryServiceController.java
+++ qanary-component-NED-DBpediaSpotlight/src/test/java/eu/wdaqua/qanary/spotlightNED/TestQanaryServiceController.java
@@ -1,155 +1,141 @@
 package eu.wdaqua.qanary.spotlightNED;
 
-import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.endpointKey;
-import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.inGraphKey;
-import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.outGraphKey;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import eu.wdaqua.qanary.commons.QanaryMessage;
+import eu.wdaqua.qanary.commons.config.QanaryConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map.Entry;
+
+import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Map.Entry;
-
-import javax.inject.Inject;
-
-import org.apache.http.client.ClientProtocolException;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import eu.wdaqua.qanary.commons.QanaryMessage;
-import eu.wdaqua.qanary.commons.config.QanaryConfiguration;
-import eu.wdaqua.qanary.component.QanaryServiceController;
-
-import com.google.gson.JsonArray;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
-@WebAppConfiguration
-public class TestQanaryServiceController {
+class TestQanaryServiceController {
 
-	private static final Logger logger = LoggerFactory.getLogger(TestQanaryServiceController.class);
+    private static final Logger logger = LoggerFactory.getLogger(TestQanaryServiceController.class);
 
-	@Inject
-	QanaryServiceController controller;
+    private MockMvc mockMvc;
 
-	private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext applicationContext;
 
-	/**
-	 * initialize local controller enabled for tests
-	 * 
-	 * @throws Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/jsp/view/");
-		viewResolver.setSuffix(".jsp");
+    /**
+     * initialize local controller enabled for tests
+     *
+     * @throws Exception
+     */
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.applicationContext).build();
+    }
 
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).setViewResolvers(viewResolver).build();
-	}
+    /**
+     * test description interface
+     *
+     * @throws Exception
+     */
+    @Test
+    void testDescriptionAvailable() throws Exception {
+        mockMvc.perform(get(QanaryConfiguration.description)) // fetch
+                .andExpect(status().isOk()) // HTTP 200
+                .andReturn(); //
+    }
 
-	/**
-	 * test description interface
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testDescriptionAvailable() throws Exception {
-		mockMvc.perform(get(QanaryConfiguration.description)) // fetch
-				.andExpect(status().isOk()) // HTTP 200
-				.andReturn(); //
-	}
+    /**
+     * send and receive message a JSON message to
+     * QanaryConfiguration.annotatequestion, check if the values are the same
+     */
+    @Test
+    @Disabled
+    // this test cannot be executed as the triplestore needs to be mocked first
+    void testMessageReceiveAndSend() {
 
-	/**
-	 * send and receive message a JSON message to
-	 * QanaryConfiguration.annotatequestion, check if the values are the same
-	 */
-	@Test
-	@Ignore // this test cannot be executed as the triplestore needs to be mocked first
-	public void testMessageReceiveAndSend() {
+        QanaryMessage requestMessage;
+        try {
+            requestMessage = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
+            logger.info("Message {}" + requestMessage);
+        } catch (URISyntaxException e) {
+            fail(e.getMessage());
+            return;
+        }
 
-		QanaryMessage requestMessage;
-		try {
-			requestMessage = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
-			logger.info("Message {}" + requestMessage);
-		} catch (URISyntaxException e) {
-			fail(e.getMessage());
-			return;
-		}
+        // check the response
+        MvcResult res;
+        try {
+            res = mockMvc.perform( //
+                            post(QanaryConfiguration.annotatequestion) //
+                                    .content(requestMessage.asJsonString()) //
+                                    .contentType(MediaType.APPLICATION_JSON))
+                    // .andExpect(status().is2xxSuccessful()) //
+                    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)) //
+                    .andReturn();
+        } catch (Exception e) {
+            fail(e.getMessage());
+            return;
+        }
 
-		// check the response
-		MvcResult res;
-		try {
-			res = mockMvc.perform( //
-					post(QanaryConfiguration.annotatequestion) //
-							.content(requestMessage.asJsonString()) //
-							.contentType(MediaType.APPLICATION_JSON))
-					// .andExpect(status().is2xxSuccessful()) //
-					.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)) //
-					.andReturn();
-		} catch (Exception e) {
-			fail(e.getMessage());
-			return;
-		}
+        // check the values of all messages, should be equal if dummy
+        // implementation is used
+        QanaryMessage resultMessage;
+        try {
+            resultMessage = new QanaryMessage(res.getResponse().getContentAsString());
+        } catch (Exception e) {
+            fail(e.getMessage());
+            return;
+        }
 
-		// check the values of all messages, should be equal if dummy
-		// implementation is used
-		QanaryMessage resultMessage;
-		try {
-			resultMessage = new QanaryMessage(res.getResponse().getContentAsString());
-		} catch (Exception e) {
-			fail(e.getMessage());
-			return;
-		}
+        for (Entry<URI, URI> entry : requestMessage.getValues().entrySet()) {
+            URI key = entry.getKey();
+//			int compareResult = entry.getValue().toString().compareTo(resultMessage.getValues().get(key).toString());
+//			assertTrue("check result vs. request: " + key, compareResult == 0);
+            assertEquals(entry.getValue().toString(), resultMessage.getValues().get(key).toString(), "check result vs. request: " + key);
+        }
 
-		for (Entry<URI, URI> entry : requestMessage.getValues().entrySet()) {
-			URI key = entry.getKey();
-			int compareResult = entry.getValue().toString().compareTo(resultMessage.getValues().get(key).toString());
-			assertTrue("check result vs. request: " + key, compareResult == 0);
-		}
+    }
 
-	}
+    /**
+     * test correct message format
+     */
+    @Test
+    void testMessageFromJson() {
+        // create message from json string
+        QanaryMessage message;
+        try {
+            message = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
 
-	/**
-	 * test correct message format
-	 */
-	@Test
-	public void testMessageFromJson() {
-		// create message from json string
-		QanaryMessage message;
-		try {
-			message = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
+            URI endpointKeyUrlFromMessage = message.getEndpoint();
+            assertNotNull(endpointKeyUrlFromMessage);
 
-			URI endpointKeyUrlFromMessage = message.getEndpoint();
-			assertNotNull(endpointKeyUrlFromMessage);
+            URI endpointKeyUrlFromHere = new URI(endpointKey);
 
-			URI endpointKeyUrlFromHere = new URI(endpointKey);
+            // TODO: more tests to ensure mechanism
+//			assertTrue(endpointKeyUrlFromHere.toString().compareTo(endpointKeyUrlFromMessage.toString()) == 0);
+            assertEquals(endpointKeyUrlFromHere.toString(), endpointKeyUrlFromMessage.toString());
 
-			// TODO: more tests to ensure mechanism
-			assertTrue(endpointKeyUrlFromHere.toString().compareTo(endpointKeyUrlFromMessage.toString()) == 0);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
 
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-
-	}
+    }
 }
