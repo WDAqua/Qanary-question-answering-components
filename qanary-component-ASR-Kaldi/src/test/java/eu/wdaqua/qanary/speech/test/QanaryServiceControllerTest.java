853d72980642438db6a4e183ec8c6314d463f6da
Calvin <dev@doernchen.eu>
2022-08-15 11:16:44 +0200

    upgrade to jUnit5

--- qanary-component-ASR-Kaldi/src/test/java/eu/wdaqua/qanary/speech/test/QanaryServiceControllerTest.java
+++ qanary-component-ASR-Kaldi/src/test/java/eu/wdaqua/qanary/speech/test/QanaryServiceControllerTest.java
@@ -1,65 +1,56 @@
 package eu.wdaqua.qanary.speech.test;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import eu.wdaqua.qanary.commons.QanaryMessage;
+import eu.wdaqua.qanary.commons.config.QanaryConfiguration;
+import eu.wdaqua.qanary.speech.Application;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map.Entry;
 
-import javax.inject.Inject;
+import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import eu.wdaqua.qanary.speech.Application;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.endpointKey;
-import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.inGraphKey;
-import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.outGraphKey;
-
-import eu.wdaqua.qanary.commons.QanaryMessage;
-import eu.wdaqua.qanary.commons.config.QanaryConfiguration;
-import eu.wdaqua.qanary.component.QanaryServiceController;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
-public class TestQanaryServiceController {
+class QanaryServiceControllerTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(TestQanaryServiceController.class);
-
-	@Inject
-	QanaryServiceController controller;
+	private static final Logger logger = LoggerFactory.getLogger(QanaryServiceControllerTest.class);
 
 	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext applicationContext;
+
 
 	/**
 	 * initialize local controller enabled for tests
 	 *
 	 * @throws Exception
 	 */
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/jsp/view/");
-		viewResolver.setSuffix(".jsp");
-
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).setViewResolvers(viewResolver).build();
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.applicationContext).build();
 	}
 
 	/**
@@ -68,7 +59,7 @@ public class TestQanaryServiceController {
 	 * @throws Exception
 	 */
 	@Test
-	public void testDescriptionAvailable() throws Exception {
+	void testDescriptionAvailable() throws Exception {
 		mockMvc.perform(get(QanaryConfiguration.description)) // fetch
 				.andExpect(status().isOk()) // HTTP 200
 				.andReturn(); //
@@ -79,7 +70,7 @@ public class TestQanaryServiceController {
 	 * QanaryConfiguration.annotatequestion, check if the values are the same
 	 */
 	@Test
-	@Ignore //TODO this test cannot be executed as the triplestore needs to be mocked first
+	@Disabled //TODO this test cannot be executed as the triplestore needs to be mocked first
 	public void testMessageReceiveAndSend() {
 
 		QanaryMessage requestMessage;
@@ -118,8 +109,7 @@ public class TestQanaryServiceController {
 
 		for (Entry<URI, URI> entry : requestMessage.getValues().entrySet()) {
 			URI key = entry.getKey();
-			int compareResult = entry.getValue().toString().compareTo(resultMessage.getValues().get(key).toString());
-			assertTrue("check result vs. request: " + key, compareResult == 0);
+			assertEquals(entry.getValue().toString(), resultMessage.getValues().get(key).toString(), "check result vs. request: " + key);
 		}
 
 	}
@@ -128,7 +118,7 @@ public class TestQanaryServiceController {
 	 * test correct message format
 	 */
 	@Test
-	public void testMessageFromJson() {
+	void testMessageFromJson() {
 		// create message from json string
 		QanaryMessage message;
 		try {
@@ -140,13 +130,13 @@ public class TestQanaryServiceController {
 			URI endpointKeyUrlFromHere = new URI(endpointKey);
 
 			// TODO: more tests to ensure mechanism
-			assertTrue(endpointKeyUrlFromHere.toString().compareTo(endpointKeyUrlFromMessage.toString()) == 0);
+			assertEquals(endpointKeyUrlFromMessage.toString(), endpointKeyUrlFromHere.toString());
 
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 
-	}
+    }
 
 }
