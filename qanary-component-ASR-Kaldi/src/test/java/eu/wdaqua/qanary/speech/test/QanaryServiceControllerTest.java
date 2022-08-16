4b39968ae069429c2ebe32745d5c4edf74774745
Calvin <dev@doernchen.eu>
2022-08-16 12:32:12 +0200

    remove not used test, cleanup

--- qanary-component-ASR-Kaldi/src/test/java/eu/wdaqua/qanary/speech/test/QanaryServiceControllerTest.java
+++ qanary-component-ASR-Kaldi/src/test/java/eu/wdaqua/qanary/speech/test/QanaryServiceControllerTest.java
@@ -4,138 +4,77 @@ import eu.wdaqua.qanary.commons.QanaryMessage;
 import eu.wdaqua.qanary.commons.config.QanaryConfiguration;
 import eu.wdaqua.qanary.speech.Application;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Map.Entry;
 
 import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
 class QanaryServiceControllerTest {
+    private static final Logger logger = LoggerFactory.getLogger(QanaryServiceControllerTest.class);
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext applicationContext;
 
-	private static final Logger logger = LoggerFactory.getLogger(QanaryServiceControllerTest.class);
+    /**
+     * initialize local controller enabled for tests
+     */
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.applicationContext).build();
+    }
 
-	private MockMvc mockMvc;
+    /**
+     * test description interface
+     *
+     * @throws Exception
+     */
+    @Test
+    void testDescriptionAvailable() throws Exception {
+        mockMvc.perform(get(QanaryConfiguration.description)) // fetch
+                .andExpect(status().isOk()) // HTTP 200
+                .andReturn(); //
+    }
 
-	@Autowired
-	private WebApplicationContext applicationContext;
+    /**
+     * test correct message format
+     */
+    @Test
+    void testMessageFromJson() {
+        // create message from json string
+        QanaryMessage message;
+        try {
+            message = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
 
+            URI endpointKeyUrlFromMessage = message.getEndpoint();
+            assertNotNull(endpointKeyUrlFromMessage);
 
-	/**
-	 * initialize local controller enabled for tests
-	 *
-	 * @throws Exception
-	 */
-	@BeforeEach
-	public void setUp() throws Exception {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.applicationContext).build();
-	}
+            URI endpointKeyUrlFromHere = new URI(endpointKey);
 
-	/**
-	 * test description interface
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	void testDescriptionAvailable() throws Exception {
-		mockMvc.perform(get(QanaryConfiguration.description)) // fetch
-				.andExpect(status().isOk()) // HTTP 200
-				.andReturn(); //
-	}
+            // TODO: more tests to ensure mechanism
+            assertEquals(endpointKeyUrlFromMessage.toString(), endpointKeyUrlFromHere.toString());
 
-	/**
-	 * send and receive message a JSON message to
-	 * QanaryConfiguration.annotatequestion, check if the values are the same
-	 */
-	@Test
-	@Disabled //TODO this test cannot be executed as the triplestore needs to be mocked first
-	public void testMessageReceiveAndSend() {
-
-		QanaryMessage requestMessage;
-		try {
-			requestMessage = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
-			logger.info("Message {}" + requestMessage);
-		} catch (URISyntaxException e) {
-			fail(e.getMessage());
-			return;
-		}
-
-		// check the response
-		MvcResult res;
-		try {
-			res = mockMvc.perform( //
-					post(QanaryConfiguration.annotatequestion) //
-							.content(requestMessage.asJsonString()) //
-							.contentType(MediaType.APPLICATION_JSON))
-					// .andExpect(status().is2xxSuccessful()) //
-					.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)) //
-					.andReturn();
-		} catch (Exception e) {
-			fail(e.getMessage());
-			return;
-		}
-
-		// check the values of all messages, should be equal if dummy
-		// implementation is used
-		QanaryMessage resultMessage;
-		try {
-			resultMessage = new QanaryMessage(res.getResponse().getContentAsString());
-		} catch (Exception e) {
-			fail(e.getMessage());
-			return;
-		}
-
-		for (Entry<URI, URI> entry : requestMessage.getValues().entrySet()) {
-			URI key = entry.getKey();
-			assertEquals(entry.getValue().toString(), resultMessage.getValues().get(key).toString(), "check result vs. request: " + key);
-		}
-
-	}
-
-	/**
-	 * test correct message format
-	 */
-	@Test
-	void testMessageFromJson() {
-		// create message from json string
-		QanaryMessage message;
-		try {
-			message = new QanaryMessage(new URI(endpointKey), new URI(inGraphKey), new URI(outGraphKey));
-
-			URI endpointKeyUrlFromMessage = message.getEndpoint();
-			assertNotNull(endpointKeyUrlFromMessage);
-
-			URI endpointKeyUrlFromHere = new URI(endpointKey);
-
-			// TODO: more tests to ensure mechanism
-			assertEquals(endpointKeyUrlFromMessage.toString(), endpointKeyUrlFromHere.toString());
-
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
 
     }
 
