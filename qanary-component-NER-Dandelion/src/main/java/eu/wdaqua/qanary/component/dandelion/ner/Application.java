86d14b98cd80ad336f1b0b71c4c7120e918ee9d9
Calvin <dev@doernchen.eu>
2022-08-23 12:34:26 +0200

    upgrade to jUnit5, update package name and increase version

--- qanary-component-NER-Dandelion/src/main/java/eu/wdaqua/qanary/component/dandelion/ner/Application.java
+++ qanary-component-NER-Dandelion/src/main/java/eu/wdaqua/qanary/component/dandelion/ner/Application.java
@@ -1,4 +1,4 @@
-package eu.wdaqua.qanary.dandelion;
+package eu.wdaqua.qanary.component.dandelion.ner;
 
 import eu.wdaqua.qanary.component.QanaryComponent;
 import org.springframework.beans.factory.annotation.Value;
--- qanary-component-NER-Dandelion/src/main/java/eu/wdaqua/qanary/component/dandelion/ner/Dandelion.java
+++ qanary-component-NER-Dandelion/src/main/java/eu/wdaqua/qanary/component/dandelion/ner/Dandelion.java
@@ -1,4 +1,4 @@
-package eu.wdaqua.qanary.dandelion;
+package eu.wdaqua.qanary.component.dandelion.ner;
 
 import eu.wdaqua.qanary.commons.QanaryMessage;
 import eu.wdaqua.qanary.commons.QanaryQuestion;
