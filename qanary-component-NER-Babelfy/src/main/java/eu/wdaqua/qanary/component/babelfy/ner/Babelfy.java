2a8e091813fb5c8991a90be37881eefc00701f24
Calvin <dev@doernchen.eu>
2022-08-23 12:05:36 +0200

    upgrade to jUnit5, update package name and increase version

--- qanary-component-NER-Babelfy/src/main/java/eu/wdaqua/qanary/component/babelfy/ner/Babelfy.java
+++ qanary-component-NER-Babelfy/src/main/java/eu/wdaqua/qanary/component/babelfy/ner/Babelfy.java
@@ -1,4 +1,4 @@
-package eu.wdaqua.qanary.mypackage;
+package eu.wdaqua.qanary.component.babelfy.ner;
 
 import eu.wdaqua.qanary.commons.QanaryMessage;
 import eu.wdaqua.qanary.commons.QanaryQuestion;
