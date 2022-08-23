9ec07dc549645bb72cec5992a9d9b8f0d9bf2315
Calvin <dev@doernchen.eu>
2022-08-23 12:44:44 +0200

    upgrade to jUnit5, update package name and increase version

--- qanary-component-NER-EntityClassifier2/src/main/java/eu/wdaqua/qanary/component/entityclassifier2/ner/Application.java
+++ qanary-component-NER-EntityClassifier2/src/main/java/eu/wdaqua/qanary/component/entityclassifier2/ner/Application.java
@@ -1,13 +1,12 @@
-package eu.wdaqua.qanary.EntityClassifier2;
+package eu.wdaqua.qanary.component.entityclassifier2.ner;
 
+import eu.wdaqua.qanary.component.QanaryComponent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import eu.wdaqua.qanary.component.QanaryComponent;
 
 @SpringBootApplication
 @EnableAutoConfiguration
