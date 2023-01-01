ba774b00c429d760978e6c25fb3385fb981ba28b
Aleksandr Perevalov <aleksandr.perevalov@hs-anhalt.de>
2023-01-01 20:07:01 +0100

    small fix of URL_COMPONENT building

--- qanary_component-EAT-Python-Answer-Type-classifier/run.py
+++ qanary_component-EAT-Python-Answer-Type-classifier/run.py
@@ -15,7 +15,7 @@ SERVICE_HOST = os.environ['SERVICE_HOST']
 SERVICE_PORT = os.environ['SERVICE_PORT']
 SERVICE_NAME_COMPONENT = os.environ['SERVICE_NAME_COMPONENT']
 SERVICE_DESCRIPTION_COMPONENT = os.environ['SERVICE_DESCRIPTION_COMPONENT']
-URL_COMPONENT = f"http://{SERVICE_HOST}:{SERVICE_PORT}"
+URL_COMPONENT = f"{SERVICE_HOST}:{SERVICE_PORT}"
 
 # define metadata that will be shown in the Spring Boot Admin server UI
 metadata = {
