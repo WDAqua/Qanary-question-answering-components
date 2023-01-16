68ee664e87b012a26b478d7eea737a6d615ffb37
Calvin <dev@doernchen.eu>
2023-01-16 16:39:39 +0100

    NED-Babelfy 3.1.4 set java version to 17 set minimal qanary version to 3.4.1 replace README.md with README.adoc move query in separated file removed unnecessary file: TagmeNED.java

--- qanary-component-NED-Babelfy/src/main/java/eu/wdaqua/qanary/component/babelfy/ned/BabelfyNED.java
+++ qanary-component-NED-Babelfy/src/main/java/eu/wdaqua/qanary/component/babelfy/ned/BabelfyNED.java
@@ -3,13 +3,18 @@ package eu.wdaqua.qanary.component.babelfy.ned;
 import eu.wdaqua.qanary.commons.QanaryMessage;
 import eu.wdaqua.qanary.commons.QanaryQuestion;
 import eu.wdaqua.qanary.commons.QanaryUtils;
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
 import eu.wdaqua.qanary.component.QanaryComponent;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.query.QuerySolutionMap;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.util.ArrayList;
 
 @Component
@@ -32,6 +37,8 @@ public class BabelfyNED extends QanaryComponent {
 
 	@Inject
 	private BabelfyConfiguration babelfyConfiguration;
+
+	private String FILENAME_INSERT_ANNOTATION = "insert_one_annotation.rq";
 
 	public BabelfyNED(@Value("${spring.application.name}") final String applicationName) {
 		this.applicationName = applicationName;
@@ -65,30 +72,24 @@ public class BabelfyNED extends QanaryComponent {
 		// TODO: implement this (custom for every component)
 
 		for (BabelfyServiceFetcher.Link l : links) {
-			String sparql = "PREFIX qa: <http://www.wdaqua.eu/qa#> " //
-					+ "PREFIX oa: <http://www.w3.org/ns/openannotation/core/> " //
-					+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " //
-					+ "INSERT { " + "GRAPH <" + myQanaryQuestion.getOutGraph() + "> { " //
-					+ "  ?a a qa:AnnotationOfInstance . " //
-					+ "  ?a oa:hasTarget [ " //
-					+ "           a    oa:SpecificResource; " //
-					+ "           oa:hasSource    <" + myQanaryQuestion.getUri() + ">; " //
-					+ "           oa:hasSelector  [ " //
-					+ "                    a oa:TextPositionSelector ; " //
-					+ "                    oa:start \"" + l.begin + "\"^^xsd:nonNegativeInteger ; " //
-					+ "                    oa:end  \"" + l.end + "\"^^xsd:nonNegativeInteger  " //
-					+ "           ] " //
-					+ "  ] . " //
-					+ "  ?a oa:hasBody <" + l.link + "> ;" //
-					+ "     oa:annotatedBy <urn:qanary:" + this.applicationName + "> ; " //
-					+ "	    oa:annotatedAt ?time  " + "}} " //
-					+ "WHERE { " //
-					+ "  BIND (IRI(str(RAND())) AS ?a) ."//
-					+ "  BIND (now() as ?time) " //
-					+ "}";
-			logger.debug("Sparql query: {}", sparql);
+			QuerySolutionMap bindingsForInsert = new QuerySolutionMap();
+			bindingsForInsert.add("graph", ResourceFactory.createResource(myQanaryQuestion.getOutGraph().toASCIIString()));
+			bindingsForInsert.add("targetQuestion", ResourceFactory.createResource(myQanaryQuestion.getUri().toASCIIString()));
+			bindingsForInsert.add("start", ResourceFactory.createTypedLiteral(String.valueOf(l.begin), XSDDatatype.XSDnonNegativeInteger));
+			bindingsForInsert.add("end", ResourceFactory.createTypedLiteral(String.valueOf(l.end), XSDDatatype.XSDnonNegativeInteger));
+			bindingsForInsert.add("answer", ResourceFactory.createStringLiteral(l.link));
+			bindingsForInsert.add("application", ResourceFactory.createResource("urn:qanary:" + this.applicationName));
+
+			// get the template of the INSERT query
+			String sparql = this.loadQueryFromFile(FILENAME_INSERT_ANNOTATION, bindingsForInsert);
+			logger.info("Sparql query {}", sparql);
 			myQanaryUtils.getQanaryTripleStoreConnector().update(sparql);
+
 		}
 		return myQanaryMessage;
+	}
+
+	private String loadQueryFromFile(String filenameWithRelativePath, QuerySolutionMap bindings) throws IOException {
+		return QanaryTripleStoreConnector.readFileFromResourcesWithMap(filenameWithRelativePath, bindings);
 	}
 }
