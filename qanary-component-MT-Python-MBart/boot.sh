b3d6973c7398326fd7771fb95c22c064c23540b9
heinpa <heipa@posteo.de>
2023-01-23 19:05:09 +0100

    read .env file in boot script

--- qanary-component-MT-Python-MBart/boot.sh
+++ qanary-component-MT-Python-MBart/boot.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo The port number is: $SERVER_PORT
-echo The Qanary pipeline URL is: $SPRING_BOOT_ADMIN_URL
+export $(grep -v '^#' .env | xargs)
+
+echo SERVER_PORT: $SERVER_PORT
+echo Qanary pipeline at SPRING_BOOT_ADMIN_URL: $SPRING_BOOT_ADMIN_URL
+
 if [ -n $SERVER_PORT ]
 then
     exec gunicorn -b :$SERVER_PORT --access-logfile - --error-logfile - run:app # refer to the gunicorn documentation for more options
