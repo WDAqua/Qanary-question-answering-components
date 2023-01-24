f1b9d5b5a286bccd975fd43758e68846ce7c875a
heinpa <heipa@posteo.de>
2023-01-24 17:25:59 +0100

    download models before gunicorn start

--- qanary-component-MT-Python-MBart/boot.sh
+++ qanary-component-MT-Python-MBart/boot.sh
@@ -2,6 +2,10 @@
 
 export $(grep -v '^#' .env | xargs)
 
+echo Downloading the model
+python -c 'from transformers import MBartForConditionalGeneration, MBart50TokenizerFast; model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt"); tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")'
+echo Downloading the model finished
+
 echo SERVER_PORT: $SERVER_PORT
 echo Qanary pipeline at SPRING_BOOT_ADMIN_URL: $SPRING_BOOT_ADMIN_URL
 
