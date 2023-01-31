6142993c693d63e9924e29362f61575714e89849
anbo-de <github@andreasboth.de>
2023-01-31 01:50:16 +0100

    NED-OpenTapico v3.2.0, added cache & code cleanup
    * switched to RestTemplate using our RestTemplateWithCaching
    * added default cache configuration (28 days)
    * moved to qanary.component v3.7.0
    * removed banner.txt and description.html as it is provided by qanary.component
    * added guarding for live tests of OpenTapioca API: use system environment variable live_tests_enabled to enable the tests in OpenTapiocaNEDLiveTest
    * additional code cleanup

--- qanary-component-NED-Opentapioca/src/main/java/eu/wdaqua/component/opentapioca/ned/Application.java
+++ qanary-component-NED-Opentapioca/src/main/java/eu/wdaqua/component/opentapioca/ned/Application.java
@@ -1,18 +1,26 @@
 package eu.wdaqua.component.opentapioca.ned;
 
+import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
 import eu.wdaqua.qanary.component.QanaryComponent;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"eu.wdaqua.qanary"})
+@EnableCaching
 public class Application {
+	
+    @Autowired
+    private RestTemplateWithCaching restTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -27,7 +35,7 @@ public class Application {
 
 	@Bean
 	public OpenTapiocaServiceFetcher openTapiocaServiceFetcher() {
-		return new OpenTapiocaServiceFetcher();
+		return new OpenTapiocaServiceFetcher(restTemplate);
 	}
 
 	@Bean
