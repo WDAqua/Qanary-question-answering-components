4ce3606f200de9bfd99fcf6eada0bc7aeb5e0171
Calvin <dev@doernchen.eu>
2023-04-17 15:58:39 +0200

    NER-Dandelion 3.2.0 split the process method in seperate testeble methods for future testing add live test add mocked test add cache test

--- qanary-component-NER-Dandelion/src/main/java/eu/wdaqua/qanary/component/dandelion/ner/Application.java
+++ qanary-component-NER-Dandelion/src/main/java/eu/wdaqua/qanary/component/dandelion/ner/Application.java
@@ -1,6 +1,11 @@
 package eu.wdaqua.qanary.component.dandelion.ner;
 
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
+import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
 import eu.wdaqua.qanary.component.QanaryComponent;
+import eu.wdaqua.qanary.component.dandelion.ner.exception.ApiLiveTestFaildException;
+import eu.wdaqua.qanary.component.dandelion.ner.exception.ApiTokenIsNullOrEmptyException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,19 +19,33 @@ import org.springframework.context.annotation.ComponentScan;
  * note: there is no need to change something here
  */
 public class Application {
+    private CacheOfRestTemplateResponse myCacheOfResponses;
+    private RestTemplateWithCaching myRestTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public Application( //
+                        @Autowired RestTemplateWithCaching myRestTemplate, //
+                        @Autowired CacheOfRestTemplateResponse myCacheOfResponses //
+    ) {
+        this.myRestTemplate = myRestTemplate;
+        this.myCacheOfResponses = myCacheOfResponses;
+    }
 
-	/**
-	 * this method is needed to make the QanaryComponent in this project known
-	 * to the QanaryServiceController in the qanary_component-template
-	 *
-	 * @return
-	 */
-	@Bean
-	public QanaryComponent qanaryComponent(@Value("${spring.application.name}") final String applicationName) {
-		return new Dandelion(applicationName);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * this method is needed to make the QanaryComponent in this project known
+     * to the QanaryServiceController in the qanary_component-template
+     *
+     * @return
+     */
+    @Bean
+    public QanaryComponent qanaryComponent(
+            @Value("${spring.application.name}") final String applicationName, //
+            @Value("${ner.dandelion.api.live.test.active}") final boolean apiLiveTestActive, //
+            @Value("${ner.dandelion.api.token}") final String apiToken //
+    ) throws ApiTokenIsNullOrEmptyException, ApiLiveTestFaildException {
+        return new Dandelion(applicationName, myRestTemplate, myCacheOfResponses, apiLiveTestActive, apiToken);
+    }
 }
