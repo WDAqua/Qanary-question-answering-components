9e10418d82855465e7d926021dc52964d2639244
Calvin <dev@doernchen.eu>
2023-04-24 14:49:06 +0200

    qanary-component-NER-DBpediaSpotlight 3.2.0 add tests update the fetcher, using now restTemplate split the process method in smaller methods

--- qanary-component-NER-DBpediaSpotlight/src/main/java/eu/wdaqua/qanary/component/dbpediaspotlight/ner/Application.java
+++ qanary-component-NER-DBpediaSpotlight/src/main/java/eu/wdaqua/qanary/component/dbpediaspotlight/ner/Application.java
@@ -1,15 +1,18 @@
 package eu.wdaqua.qanary.component.dbpediaspotlight.ner;
 
 import com.google.gson.JsonArray;
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
 import eu.wdaqua.qanary.component.QanaryComponent;
 import eu.wdaqua.qanary.component.dbpediaspotlight.ner.exceptions.DBpediaSpotlightServiceNotAvailable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by didier on 27.03.16.
@@ -17,61 +20,88 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"eu.wdaqua.qanary"})
 public class Application {
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	/**
-	 * this method is needed to make the QanaryComponent in this project known to
-	 * the QanaryServiceController in the qanary_component-template
-	 */
-	@Bean
-	public QanaryComponent qanaryComponent( //
-											@Value("${spring.application.name}") final String applicationName, //
-											DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration, //
-											DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher //
-	) {
-		return new DBpediaSpotlightNER(applicationName, dBpediaSpotlightConfiguration, dBpediaSpotlightServiceFetcher);
-	}
+    /**
+     * this method is needed to make the QanaryComponent in this project known to
+     * the QanaryServiceController in the qanary_component-template
+     */
+    @Bean
+    public QanaryComponent qanaryComponent(
+            @Value("${spring.application.name}") final String applicationName, //
+            DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration, //
+            DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher, //
+            @Autowired RestTemplate myRestTemplate, //
+            @Autowired CacheOfRestTemplateResponse myCacheOfRestTemplateResponse //
+    ) {
+        return new DBpediaSpotlightNER(
+                applicationName, //
+                dBpediaSpotlightConfiguration, //
+                dBpediaSpotlightServiceFetcher, //
+                myRestTemplate, //
+                myCacheOfRestTemplateResponse //
+        );
+    }
 
-	@Bean
-	public DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration( //
-																		@Value("${dbpediaspotlight.test-question}") String testQuestion, //
-																		@Value("${dbpediaspotlight.confidence.minimum}") float confidenceMinimum, //
-																		@Value("${dbpediaspotlight.endpoint:https://api.dbpedia-spotlight.org/en/annotate}") String endpoint, //
-																		DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher, //
-																		@Value("${dbpediaspotlight.endpoint.check.availability:false}") boolean checkAvailability //
-	) throws DBpediaSpotlightServiceNotAvailable {
-		this.checkSpotlightServiceAvailability(testQuestion, endpoint, confidenceMinimum,
-				dBpediaSpotlightServiceFetcher, checkAvailability);
-		logger.debug("endpoint: {}", endpoint);
-		return new DBpediaSpotlightConfiguration(confidenceMinimum, endpoint);
-	}
+    @Bean
+    public DBpediaSpotlightConfiguration dBpediaSpotlightConfiguration(
+            @Value("${dbpediaspotlight.test-question}") String testQuestion, //
+            @Value("${dbpediaspotlight.confidence.minimum}") float confidenceMinimum, //
+            @Value("${dbpediaspotlight.endpoint:https://api.dbpedia-spotlight.org/en/annotate}") String endpoint, //
+            DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher, //
+            @Value("${dbpediaspotlight.endpoint.check.availability:false}") boolean checkAvailability, //
+            @Autowired RestTemplate myRestTemplate, //
+            @Autowired CacheOfRestTemplateResponse myCacheOfRestTemplateResponse //
+    ) throws DBpediaSpotlightServiceNotAvailable {
+        this.checkSpotlightServiceAvailability(
+                testQuestion, //
+                endpoint, //
+                confidenceMinimum, //
+                dBpediaSpotlightServiceFetcher, //
+                checkAvailability, //
+                myRestTemplate, //
+                myCacheOfRestTemplateResponse //
+        );
+        LOGGER.debug("endpoint: {}", endpoint);
+        return new DBpediaSpotlightConfiguration(confidenceMinimum, endpoint);
+    }
 
-	@Bean
-	DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher() {
-		return new DBpediaSpotlightServiceFetcher();
-	}
+    @Bean
+    DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher() {
+        return new DBpediaSpotlightServiceFetcher();
+    }
 
-	private void checkSpotlightServiceAvailability(String testQuestion, String endpoint, float confidenceMinimum,
-												   DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher,
-												   boolean checkAvailability
-	) throws DBpediaSpotlightServiceNotAvailable {
-		if (checkAvailability == false) {
-			logger.warn("Availability check of DBpedia Spotlight service is disabled!");
-			return;
-		}
+    private void checkSpotlightServiceAvailability(
+            String testQuestion,
+            String endpoint,
+            float confidenceMinimum,
+            DBpediaSpotlightServiceFetcher dBpediaSpotlightServiceFetcher,
+            boolean checkAvailability,
+            RestTemplate myRestTemplate, //
+            CacheOfRestTemplateResponse myCacheOfRestTemplateResponse //
+    ) throws DBpediaSpotlightServiceNotAvailable {
+        if (checkAvailability == false) {
+            LOGGER.warn("Availability check of DBpedia Spotlight service is disabled!");
+            return;
+        }
 
-		String err;
-		try {
-			JsonArray response = dBpediaSpotlightServiceFetcher.getJsonFromService(testQuestion, endpoint,
-					confidenceMinimum);
-			return;
-		} catch (Exception e) {
-			err = e.getLocalizedMessage();
-		}
-		throw new DBpediaSpotlightServiceNotAvailable("No response from endpoint " + endpoint + "!\n" + err);
-	}
+        String err;
+        try {
+            JsonArray response = dBpediaSpotlightServiceFetcher.getJsonFromService(
+                    testQuestion, //
+                    endpoint, //
+                    confidenceMinimum, //
+                    myRestTemplate, //
+                    myCacheOfRestTemplateResponse //
+            );
+            return;
+        } catch (Exception e) {
+            err = e.getLocalizedMessage();
+        }
+        throw new DBpediaSpotlightServiceNotAvailable("No response from endpoint " + endpoint + "!\n" + err);
+    }
 }
---
+++ qanary-component-NER-DBpediaSpotlight/src/test/java/eu/wdaqua/qanary/component/dbpediaspotlight/ner/DBpediaSpotlightTestConfiguration.java
@@ -0,0 +1,27 @@
+package eu.wdaqua.qanary.component.dbpediaspotlight.ner;
+
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
+import org.springframework.boot.test.context.TestConfiguration;
+
+import java.io.IOException;
+
+@TestConfiguration
+public class DBpediaSpotlightTestConfiguration {
+    // define here the current CaffeineCacheManager configuration
+    static {
+        System.setProperty("qanary.webservicecalls.cache.specs", "maximumSize=1000,expireAfterAccess=" + DBpediaSpotlightServiceFetcherTest.MAX_TIME_SPAN_SECONDS + "s");
+        // deactivate the live test of the real-world webservice
+        System.setProperty("dbpediaspotlight.perform-live-check-on-component-start", "false");
+    }
+
+    /**
+     * get the defined SPARQL query and remove all control characters (like newline)
+     *
+     * @return
+     * @throws IOException
+     */
+    protected static String getTestQuery(String testQueryFilename) throws IOException {
+        return QanaryTripleStoreConnector.readFileFromResources(testQueryFilename).replaceAll("\\p{Cntrl}", "");
+    }
+
+}
