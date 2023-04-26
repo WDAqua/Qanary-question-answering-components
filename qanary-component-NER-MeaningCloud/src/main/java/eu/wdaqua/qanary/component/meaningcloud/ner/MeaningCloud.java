1435205e9461684b0dfe238e3a701aba365760a8
Calvin <dev@doernchen.eu>
2023-04-26 16:14:47 +0200

    NER-MeaningCloud 3.2.0 add live tests add mocked tests add cache tests split process method in separate methods changed api call to restTemplate (with cache) remove default template bugfix in insert query

--- qanary-component-NER-MeaningCloud/src/main/java/eu/wdaqua/qanary/component/meaningcloud/ner/MeaningCloud.java
+++ qanary-component-NER-MeaningCloud/src/main/java/eu/wdaqua/qanary/component/meaningcloud/ner/MeaningCloud.java
@@ -1,28 +1,37 @@
 package eu.wdaqua.qanary.component.meaningcloud.ner;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import eu.wdaqua.qanary.commons.QanaryExceptionNoOrMultipleQuestions;
 import eu.wdaqua.qanary.commons.QanaryMessage;
 import eu.wdaqua.qanary.commons.QanaryQuestion;
 import eu.wdaqua.qanary.commons.QanaryUtils;
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
+import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
 import eu.wdaqua.qanary.component.QanaryComponent;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import eu.wdaqua.qanary.component.meaningcloud.ner.exception.ApiLiveTestFaildException;
+import eu.wdaqua.qanary.component.meaningcloud.ner.exception.ApiTokenIsNullOrEmptyException;
+import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.query.QuerySolutionMap;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.shiro.util.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 
 @Component
 /**
@@ -34,15 +43,51 @@ import java.util.ArrayList;
  *      target="_top">Github wiki howto</a>
  */
 public class MeaningCloud extends QanaryComponent {
-    private static final Logger logger = LoggerFactory.getLogger(MeaningCloud.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MeaningCloud.class);
+
+    private final String FILENAME_INSERT_ANNOTATION = "/queries/insert_one_annotation.rq";
 
     private final String applicationName;
+    private final String meaningCloudKey;
+    private final RestTemplateWithCaching myRestTemplate;
+    private final CacheOfRestTemplateResponse myCacheOfResponses;
 
-    @Value("${meaningcloud.key}")
-    private String meaningCloudKey;
+    public MeaningCloud(
+            @Value("${spring.application.name}") final String applicationName, //
+            @Value("${meaningcloud.api.live.test.active}") final boolean apiLiveTestActive, //
+            @Value("${meaningcloud.api.key}") String meaningCloudKey, //
+            @Autowired RestTemplateWithCaching myRestTemplate, //
+            @Autowired CacheOfRestTemplateResponse myCacheOfResponses //
+    ) throws ApiTokenIsNullOrEmptyException, ApiLiveTestFaildException {
+        if (meaningCloudKey == null || meaningCloudKey.isEmpty()) {
+            throw new ApiTokenIsNullOrEmptyException();
+        }
 
-    public MeaningCloud(@Value("${spring.application.name}") final String applicationName) {
         this.applicationName = applicationName;
+        this.meaningCloudKey = meaningCloudKey;
+        this.myRestTemplate = myRestTemplate;
+        this.myCacheOfResponses = myCacheOfResponses;
+
+        // check if files exists and are not empty
+        QanaryTripleStoreConnector.guardNonEmptyFileFromResources(FILENAME_INSERT_ANNOTATION);
+
+        if (apiLiveTestActive) {
+            LOGGER.info("API live testing is activated");
+
+            for (int i = 0; i < 10; i++) {
+                try {
+                    this.testFunctionality();
+                    LOGGER.info("Functionality works as expected");
+                    break;
+                } catch (Exception ex) {
+                    LOGGER.warn("Functionality did not work as expected on attempt no. {}: {}", i, ex.toString());
+                    if (i > 8) {
+                        LOGGER.error("Functionality does not work as expected. Exiting..");
+                        throw new ApiLiveTestFaildException("Could not start component, {}" + applicationName);
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -53,91 +98,132 @@ public class MeaningCloud extends QanaryComponent {
      */
     @Override
     public QanaryMessage process(QanaryMessage myQanaryMessage) throws Exception {
-        logger.info("process: {}", myQanaryMessage);
-        // TODO: implement processing of question
+        LOGGER.info("process: {}", myQanaryMessage);
 
+        // STEP 1: get the required data from the Qanary triplestore (the global process memory)
         QanaryUtils myQanaryUtils = this.getUtils(myQanaryMessage);
-        QanaryQuestion<String> myQanaryQuestion = new QanaryQuestion(myQanaryMessage, myQanaryUtils.getQanaryTripleStoreConnector());
+        QanaryQuestion<String> myQanaryQuestion = this.getQanaryQuestion(myQanaryMessage);
         String myQuestion = myQanaryQuestion.getTextualRepresentation();
-        //String myQuestion = "Is Michelle Obama is the wife of Barack Obama ?";
-        ArrayList<Selection> selections = new ArrayList<Selection>();
-        logger.info("Question {}", myQuestion);
 
-        String thePath = "";
-        thePath = URLEncoder.encode(myQuestion, "UTF-8");
-        logger.info("Path {}", thePath);
 
-        HttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpget = new HttpGet(
-                "https://api.meaningcloud.com/topics-2.0?key=" + meaningCloudKey + "&of=json&lang=en&ilang=en&txt="
-                        + thePath + "&tt=e&uw=y");
-        // httpget.addHeader("User-Agent", USER_AGENT);
-        HttpResponse response = httpclient.execute(httpget);
-        try {
+        // STEP 2: enriching of query and fetching data from the Dandelion API
+        LOGGER.info("Question {}", myQuestion);
 
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                InputStream instream = entity.getContent();
-                String text = IOUtils.toString(instream, StandardCharsets.UTF_8.name());
-                JSONObject response2 = new JSONObject(text);
-                logger.info("response2: {}", response2);
-                if (response2.has("entity_list")) {
-                    JSONArray ents = (JSONArray) response2.get("entity_list");
-                    for (int j = 0; j < ents.length(); j++) {
-                        JSONObject formObject = (JSONObject) ents.getJSONObject(j);
-                        logger.info("formObject_1: {}", formObject);
-                        if (formObject.has("variant_list")) {
-                            JSONArray jsonArray = (JSONArray) formObject.get("variant_list");
-                            logger.info("jsonArray_variant_list : {}", jsonArray);
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject explrObject = jsonArray.getJSONObject(i);
-                                logger.info("form_explrObject_2 : {}", explrObject);
-                                int begin = explrObject.getInt("inip");
-                                int end = explrObject.getInt("endp");
-                                logger.info("Question: {}", explrObject);
-                                logger.info("Begin: {}", begin);
-                                logger.info("End: {}", end);
-                                Selection s = new Selection();
-                                s.begin = begin;
-                                s.end = end;
-                                selections.add(s);
-                            }
-                        }
+        JsonObject apiResponse = this.sendRequestToAPI(myQuestion);
+        List<Selection> selections = this.getSelectionsFromAnnotation(apiResponse);
+
+
+        // STEP 3: add information to Qanary triplestore
+        this.updateTriplestore(selections, myQanaryQuestion, myQanaryUtils);
+
+        return myQanaryMessage;
+    }
+
+    public JsonObject sendRequestToAPI(String myQuestion) throws UnsupportedEncodingException, URISyntaxException {
+        StringBuilder uriBuilder = new StringBuilder();
+        uriBuilder.append("https://api.meaningcloud.com/topics-2.0");
+        uriBuilder.append("?key=").append(meaningCloudKey);
+        uriBuilder.append("&of=json&lang=en&ilang=en");
+        uriBuilder.append("&txt=").append(URLEncoder.encode(myQuestion, "UTF-8"));
+        uriBuilder.append("&tt=e&uw=y");
+        URI uri = new URI(uriBuilder.toString());
+
+        long requestBefore = this.myCacheOfResponses.getNumberOfExecutedRequests();
+
+        LOGGER.debug("URL: {}", uri);
+        ResponseEntity<String> response = myRestTemplate.getForEntity(uri, String.class);
+
+        //TODO: check if response is valid
+        Assert.notNull(response);
+        Assert.notNull(response.getBody());
+
+        if (myCacheOfResponses.getNumberOfExecutedRequests() - requestBefore == 0) {
+            LOGGER.warn("request was cached: {}", uri);
+        } else {
+            LOGGER.info("request was actually executed: {}", uri);
+        }
+
+        JsonObject jsonObject = JsonParser.parseString(response.getBody()).getAsJsonObject();
+        LOGGER.debug("JSON: {}", jsonObject);
+
+        return jsonObject;
+    }
+
+    public List<Selection> getSelectionsFromAnnotation(JsonObject jsonObject) {
+        ArrayList<Selection> selections = new ArrayList<>();
+
+        if (jsonObject.has("entity_list")) {
+            JsonArray ents = jsonObject.getAsJsonArray("entity_list");
+            for (int j = 0; j < ents.size(); j++) {
+                JsonObject formObject = ents.get(j).getAsJsonObject();
+                LOGGER.debug("formObject_1: {}", formObject);
+
+                if (formObject.has("variant_list")) {
+                    JsonArray jsonArray = formObject.getAsJsonArray("variant_list");
+                    LOGGER.debug("jsonArray_variant_list : {}", jsonArray);
+
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        JsonObject explrObject = jsonArray.get(i).getAsJsonObject();
+                        LOGGER.debug("form_explrObject_2 : {}", explrObject);
+
+                        int begin = explrObject.get("inip").getAsInt();
+                        int end = explrObject.get("endp").getAsInt();
+
+                        LOGGER.debug("Question: {}", explrObject);
+                        LOGGER.debug("Begin: {}", begin);
+                        LOGGER.debug("End: {}", end);
+
+                        Selection s = new Selection();
+                        s.begin = begin;
+                        s.end = end;
+                        selections.add(s);
                     }
                 }
             }
-        } catch (ClientProtocolException e) {
-            logger.error("Exception: {}", e);
-            // TODO Auto-generated catch block
-        } catch (IOException e1) {
-            logger.error("Except: {}", e1);
-            // TODO Auto-generated catch block
         }
-        logger.info("store data in graph {}", myQanaryMessage.getValues().get(myQanaryMessage.getEndpoint()));
-        // TODO: insert data in QanaryMessage.outgraph
 
-        logger.info("apply vocabulary alignment on outgraph");
-        // TODO: implement this (custom for every component)
+        return selections;
+    }
+
+    public String getSparqlInsertQuery(
+            Selection selection, //
+            QanaryQuestion<String> myQanaryQuestion //
+    ) throws QanaryExceptionNoOrMultipleQuestions, URISyntaxException, SparqlQueryFailed, IOException {
+        QuerySolutionMap bindingsForInsert = new QuerySolutionMap();
+        bindingsForInsert.add("graph", ResourceFactory.createResource(myQanaryQuestion.getOutGraph().toASCIIString()));
+        bindingsForInsert.add("targetQuestion", ResourceFactory.createResource(myQanaryQuestion.getUri().toASCIIString()));
+        bindingsForInsert.add("start", ResourceFactory.createTypedLiteral(String.valueOf(selection.begin), XSDDatatype.XSDnonNegativeInteger));
+        bindingsForInsert.add("end", ResourceFactory.createTypedLiteral(String.valueOf(selection.end), XSDDatatype.XSDnonNegativeInteger));
+        bindingsForInsert.add("application", ResourceFactory.createResource("urn:qanary:" + this.applicationName));
+
+        // get the template of the INSERT query
+        String sparql = QanaryTripleStoreConnector.readFileFromResourcesWithMap(FILENAME_INSERT_ANNOTATION, bindingsForInsert);
+        LOGGER.info("SPARQL query: {}", sparql);
+
+        return sparql;
+    }
+
+    private void updateTriplestore(
+            List<Selection> selections, //
+            QanaryQuestion<String> myQanaryQuestion, //
+            QanaryUtils myQanaryUtils //
+    ) throws QanaryExceptionNoOrMultipleQuestions, URISyntaxException, SparqlQueryFailed, IOException {
         for (Selection s : selections) {
+            String sparql = this.getSparqlInsertQuery(s, myQanaryQuestion);
 
-
-
-            String sparql = "prefix qa: <http://www.wdaqua.eu/qa#> "
-                    + "prefix oa: <http://www.w3.org/ns/openannotation/core/> "
-                    + "prefix xsd: <http://www.w3.org/2001/XMLSchema#> " + "INSERT { " + "GRAPH <"
-                    + myQanaryMessage.getOutGraph() + "> { " + "  ?a a qa:AnnotationOfSpotInstance . "
-                    + "  ?a oa:hasTarget [ "
-                    + " a oa:SpecificResource; " + " oa:hasSource <" +
-                    myQanaryQuestion.getUri() + ">; "
-                    + "           oa:hasSelector  [ " + "                    a oa:TextPositionSelector ; "
-                    + "                    oa:start \"" + s.begin + "\"^^xsd:nonNegativeInteger ; "
-                    + "                    oa:end  \"" + s.end + "\"^^xsd:nonNegativeInteger  " + "           ] "
-                    + "  ] ; " + "     oa:annotatedBy <urn:qanary:" + this.applicationName + "> ; "
-                    + "	    oa:annotatedAt ?time  " + "}} " + "WHERE { " + "BIND (IRI(str(RAND())) AS ?a) ."
-                    + "BIND (now() as ?time) " + "}";
             myQanaryUtils.getQanaryTripleStoreConnector().update(sparql);
         }
-        return myQanaryMessage;
+    }
+
+    private void testFunctionality() throws UnsupportedEncodingException, URISyntaxException, ApiLiveTestFaildException {
+        String myQuestion = "Is Michelle Obama is the wife of Barack Obama?";
+
+        JsonObject jsonObject = this.sendRequestToAPI(myQuestion);
+        List<Selection> selections = this.getSelectionsFromAnnotation(jsonObject);
+
+        if (selections.isEmpty()) {
+            throw new ApiLiveTestFaildException("No selections found");
+        }
     }
 
     class Selection {
---
+++ qanary-component-NER-MeaningCloud/src/test/java/eu/wdaqua/qanary/component/meaningcloud/ner/MeaningCloudLiveTest.java
@@ -0,0 +1,156 @@
+package eu.wdaqua.qanary.component.meaningcloud.ner;
+
+import com.google.gson.JsonObject;
+import eu.wdaqua.qanary.commons.QanaryExceptionNoOrMultipleQuestions;
+import eu.wdaqua.qanary.commons.QanaryQuestion;
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
+import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
+import eu.wdaqua.qanary.component.meaningcloud.ner.exception.ApiLiveTestFaildException;
+import eu.wdaqua.qanary.component.meaningcloud.ner.exception.ApiTokenIsNullOrEmptyException;
+import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static eu.wdaqua.qanary.commons.config.QanaryConfiguration.endpointKey;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+class MeaningCloudLiveTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MeaningCloudLiveTest.class);
+
+    private final String applicationName;
+    private final boolean apiLiveTestActive;
+    private final String meaningCloudKey;
+    private final RestTemplateWithCaching myRestTemplate;
+    private final CacheOfRestTemplateResponse myCacheOfResponses;
+    private final Environment env;
+
+    private MeaningCloud meaningCloud;
+    private QanaryQuestion mockedQanaryQuestion;
+
+    MeaningCloudLiveTest(
+            @Value("${spring.application.name}") final String applicationName, //
+            @Value("${meaningcloud.api.live.test.active}") final boolean apiLiveTestActive, //
+            @Value("${meaningcloud.api.key}") String meaningCloudKey, //
+            @Autowired RestTemplateWithCaching myRestTemplate, //
+            @Autowired CacheOfRestTemplateResponse myCacheOfResponses, //
+            @Autowired Environment env
+    ) {
+        this.applicationName = applicationName;
+        this.apiLiveTestActive = apiLiveTestActive;
+        this.meaningCloudKey = meaningCloudKey;
+        this.myRestTemplate = myRestTemplate;
+        this.myCacheOfResponses = myCacheOfResponses;
+        this.env = env;
+    }
+
+    @BeforeEach
+    public void init() throws ApiLiveTestFaildException, ApiTokenIsNullOrEmptyException, QanaryExceptionNoOrMultipleQuestions, URISyntaxException, SparqlQueryFailed {
+        this.meaningCloud = new MeaningCloud(
+                applicationName,
+                false,
+                meaningCloudKey,
+                myRestTemplate,
+                myCacheOfResponses
+        );
+
+        this.mockedQanaryQuestion = Mockito.mock(QanaryQuestion.class);
+        Mockito.when(this.mockedQanaryQuestion.getOutGraph()).thenReturn(new URI(endpointKey));
+        Mockito.when(this.mockedQanaryQuestion.getUri()).thenReturn(new URI("targetquestion"));
+    }
+
+    @Test
+    @EnabledIf(
+            expression = "#{environment['meaningcloud.api.live.test.active'] == 'true'}", //
+            loadContext = true
+    )
+    void testQuestion1()
+            throws QanaryExceptionNoOrMultipleQuestions, URISyntaxException, IOException, SparqlQueryFailed {
+
+        JsonObject response = this.meaningCloud.sendRequestToAPI(this.env.getProperty("question1"));
+        assertNotNull(response);
+        assertTrue(response.has("entity_list"));
+        assertNotEquals(0, response.get("entity_list").getAsJsonArray().size());
+
+        assertTrue(response.get("entity_list").getAsJsonArray().toString().contains("Albert Einstein"));
+
+        List<MeaningCloud.Selection> selections = this.meaningCloud.getSelectionsFromAnnotation(response);
+        assertNotNull(selections);
+        assertNotEquals(0, selections.size());
+
+        for (MeaningCloud.Selection s : selections) {
+            String sparql = this.meaningCloud.getSparqlInsertQuery(s, this.mockedQanaryQuestion);
+            assertNotNull(sparql);
+            assertNotEquals(0, sparql.length());
+        }
+    }
+
+    @Test
+    @EnabledIf(
+            expression = "#{environment['meaningcloud.api.live.test.active'] == 'true'}", //
+            loadContext = true
+    )
+    void testQuestion2()
+            throws QanaryExceptionNoOrMultipleQuestions, URISyntaxException, IOException, SparqlQueryFailed {
+        JsonObject response = this.meaningCloud.sendRequestToAPI(this.env.getProperty("question2"));
+        assertNotNull(response);
+        assertTrue(response.has("entity_list"));
+        assertNotEquals(0, response.get("entity_list").getAsJsonArray().size());
+
+        assertTrue(response.get("entity_list").getAsJsonArray().toString().contains("Germany"));
+
+        List<MeaningCloud.Selection> selections = this.meaningCloud.getSelectionsFromAnnotation(response);
+        assertNotNull(selections);
+        assertNotEquals(0, selections.size());
+
+        for (MeaningCloud.Selection s : selections) {
+            String sparql = this.meaningCloud.getSparqlInsertQuery(s, this.mockedQanaryQuestion);
+            assertNotNull(sparql);
+            assertNotEquals(0, sparql.length());
+        }
+    }
+
+    @Test
+    @EnabledIf(
+            expression = "#{environment['meaningcloud.api.live.test.active'] == 'true'}", //
+            loadContext = true
+    )
+    void testQuestion3()
+            throws QanaryExceptionNoOrMultipleQuestions, URISyntaxException, SparqlQueryFailed, IOException {
+        JsonObject response = this.meaningCloud.sendRequestToAPI(this.env.getProperty("question3"));
+        assertNotNull(response);
+        assertTrue(response.has("entity_list"));
+        assertNotEquals(0, response.get("entity_list").getAsJsonArray().size());
+
+        assertTrue(response.get("entity_list").getAsJsonArray().toString().contains("Batman"));
+
+        List<MeaningCloud.Selection> selections = this.meaningCloud.getSelectionsFromAnnotation(response);
+        assertNotNull(selections);
+        assertNotEquals(0, selections.size());
+
+        for (MeaningCloud.Selection l : selections) {
+            String sparql = this.meaningCloud.getSparqlInsertQuery(l, this.mockedQanaryQuestion);
+            assertNotNull(sparql);
+            assertNotEquals(0, sparql.length());
+        }
+    }
+
+}
