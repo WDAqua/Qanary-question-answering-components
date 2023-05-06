563779662e3281edc576b3425478c50e6e0228f9
caldoe <60075860+caldoe@users.noreply.github.com>
2023-05-06 02:40:47 +0200

    TQA-ChatGPTWrapper 0.1.18 (#264)
    * TQA-ChatGPTWrapper 0.1.0
    initial creation of the component
    
    * TQA-ChatGPTWrapper 0.1.1
    some fixes in ther properties
    
    * add application-local.properties to .gitignore
    
    * TQA-ChatGPTWrapper 0.1.2
    Change annotation in insert query to `AnnotationOfTextAnswerJson`
    
    * TQA-ChatGPTWrapper 0.1.3
    Change rdf:type of answer in insert query to `ChatGptAnswerJson`
    
    * TQA-ChatGPTWrapper 0.1.4
    Add application name to `annotatedBy`
    
    * TQA-ChatGPTWrapper 0.1.5
    Change `hasTarget` value in test insert query to a real value
    
    * TQA-ChatGPTWrapper 0.1.6
    Change springdoc.version to 1.6.12
    
    * TQA-ChatGPTWrapper 0.1.7
    Remove separate caching variable `chatgpt.request.caching`
    
    * TQA-ChatGPTWrapper 0.1.8
    Increase caching time for more stable tests
    
    * TQA-ChatGPTWrapper 0.1.9
    rename property `chatgpt.key` to `tqa.chatgptwrapper.api.key`
    
    * TQA-ChatGPTWrapper 0.1.10
    move query file name to constant
    add check for query file
    
    * TQA-ChatGPTWrapper 0.1.11
    add more cache time for test
    
    * TQA-ChatGPTWrapper 0.1.12
    remove real question url from test and add a placeholder
    
    * TQA-ChatGPTWrapper 0.1.13
    add REST controller
    
    * ChatGPTWrapper: code cleanup
    
    * added `chatgpt.base.url` configuration
    * improved test code
    * increased swagger version
    
    * TQA-ChatGPTWrapper 0.1.15
    add configuration information to the README.adoc
    
    * TQA-ChatGPTWrapper 0.1.16
    change api.key property
    change api.live.test property
    
    * TQA-ChatGPTWrapper 0.1.17
    remove typo in Swagger UI
    update Swagger UI information
    add more configurations for ChatGPT request (REST Controller)
    add more configurations for ChatGPT request (Qanary Component)
    update cache Test
    update Documentation
    
    * Qanary ChatGPT v0.1.18, release
    
    ---------
    
    Co-authored-by: anbo-de <github@andreasboth.de>

---
+++ qanary-component-TQA-ChatGPTWrapper/src/test/java/eu/wdaqua/qanary/component/chatgptwrapper/tqa/ChatGPTRestControllerTest.java
@@ -0,0 +1,78 @@
+package eu.wdaqua.qanary.component.chatgptwrapper.tqa;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import eu.wdaqua.qanary.component.chatgptwrapper.tqa.controller.QuestionRestController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+class ChatGPTRestControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext applicationContext;
+
+    /**
+     * initialize local application for tests
+     */
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.applicationContext).build();
+    }
+
+
+    @Test
+    void testGetResponse() throws Exception {
+        mockMvc.perform(post("/"))
+                .andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    @EnabledIf(
+            expression = "#{environment['chatgpt.api.live.test.active'] == 'true'}", //
+            loadContext = true
+    )
+    void testPostResponse() throws Exception {
+        this.mockMvc.perform(post(QuestionRestController.ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"prompt\":\"this is a test\"}"))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void testPostResponseWithoutQuestion() throws Exception {
+        this.mockMvc.perform(post(QuestionRestController.ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testPutResponse() throws Exception {
+        mockMvc.perform(put(QuestionRestController.ENDPOINT))
+                .andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    void testDeleteResponse() throws Exception {
+        mockMvc.perform(delete(QuestionRestController.ENDPOINT))
+                .andExpect(status().isMethodNotAllowed());
+    }
+}
