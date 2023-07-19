fe6cc02dcc1ea8f3392b71c05452ddddba09b30c
heinpa <heipa@posteo.de>
2023-07-19 17:05:12 +0200

    add logging if no graph found

--- qanary-component-CopyValuesOfPriorGraph/src/main/java/eu/wdaqua/qanary/component/copyvaluesofpriorgraph/CopyValuesOfPriorGraph.java
+++ qanary-component-CopyValuesOfPriorGraph/src/main/java/eu/wdaqua/qanary/component/copyvaluesofpriorgraph/CopyValuesOfPriorGraph.java
@@ -26,13 +26,8 @@ import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
  * @see <a href="https://github.com/WDAqua/Qanary/wiki/How-do-I-integrate-a-new-component-in-Qanary%3F" target="_top">Github wiki howto</a>
  */
 public class CopyValuesOfPriorGraph extends QanaryComponent {
-	// you might use this parameterizable file to store the query that should be
-	// executed for fetching the annotations required for this component from the
-	// Qanary triplestore
+
 	private static final String FILENAME_FETCH_REQUIRED_ANNOTATIONS = "/queries/fetchRequiredAnnotations.rq";
-	// you might use this parameterizable file to store the query that should be
-	// executed for storing the annotations computed for this component from the
-	// Qanary triplestore
 	private static final String FILENAME_ADD_DATA_TO_CURRENT_GRAPH = "/queries/addDataToCurrentGraph.rq";
 	private static final String FILENAME_STORE_COMPUTED_ANNOTATIONS = "/queries/storeComputedAnnotations.rq";
 	
@@ -65,27 +60,28 @@ public class CopyValuesOfPriorGraph extends QanaryComponent {
 		// --------------------------------------------------------------------
 		// STEP 1: get the required data from the Qanary triplestore (the global process memory)
 		// --------------------------------------------------------------------
-		// if required, then fetch the origin question (here the question is a
-		// textual/String question)
 		QanaryQuestion<String> myQanaryQuestion = this.getQanaryQuestion();
-
-		// retrieve the data you need to implement your component's functionality
-
 		// check if a prior conversation was annotated
 		QuerySolutionMap bindingsForSelect = new QuerySolutionMap();
 		bindingsForSelect.add("graph", ResourceFactory.createResource(myQanaryQuestion.getInGraph().toASCIIString()));
-
 		String sparqlSelectQuery = QanaryTripleStoreConnector.readFileFromResourcesWithMap(FILENAME_FETCH_REQUIRED_ANNOTATIONS, bindingsForSelect);		
 		logger.info("generated SPARQL INSERT query: {}", sparqlSelectQuery);
         ResultSet resultset = connectorToQanaryTriplestore.select(sparqlSelectQuery);
+		int p = 0;
 		while (resultset.hasNext()) {
 			// update the current graph with information from prior graph
 			QuerySolution tuple = resultset.next();
-			String priorGraph = tuple.get("priorGraph").asResource().getURI(); // TODO: what is the correct type here? 
+			String priorGraph = tuple.get("priorGraph").asResource().getURI(); 
 			// create an update query to copy values
 			addDataToCurrentGraph(priorGraph, myQanaryQuestion, myQanaryUtils);
+			logger.info("Copied data from prior graph {}", priorGraph);
+			p++;
 		}
-		logger.info("no more data to copy!");
+		if (p == 0) {
+			logger.warn("Component {} was called, but no prior graph could be found!", applicationName);
+		} else if (p > 1) {
+		logger.warn("Copied information from more than one prior graph! ({} total)", p);
+		}
 
 		return myQanaryMessage;
 	}
