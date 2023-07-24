8aced9f3a895a5075e40701d28c22fc3d39efc95
Calvin <dev@doernchen.eu>
2023-07-24 16:53:37 +0200

    NER-DBpediaSpotlight 3.2.2
    remove warnings in pom
    move cache test in seperate file
    update cache test to the newest version
    update path to main class

--- qanary-component-NER-DBpediaSpotlight/src/test/java/eu/wdaqua/qanary/component/dbpediaspotlight/ner/DBpediaSpotlightServiceFetcherTest.java
+++ qanary-component-NER-DBpediaSpotlight/src/test/java/eu/wdaqua/qanary/component/dbpediaspotlight/ner/DBpediaSpotlightServiceFetcherTest.java
@@ -7,25 +7,19 @@ import com.google.gson.JsonParser;
 import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
 import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
 import eu.wdaqua.qanary.component.dbpediaspotlight.ner.exceptions.DBpediaSpotlightJsonParsingNotPossible;
-import jakarta.json.Json;
-import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,108 +40,6 @@ class DBpediaSpotlightServiceFetcherTest {
         // deactivate the live test of the real-world webservice
         System.setProperty("dbpediaspotlight.perform-live-check-on-component-start", "false");
         System.setProperty("dbpediaspotlight.endpoint.ssl.certificatevalidation.ignore", "false");
-    }
-
-    @BeforeEach
-    public void init() throws URISyntaxException {
-        assert this.restTemplate != null : "restTemplate cannot be null";
-    }
-
-    /**
-     * @throws InterruptedException
-     * @throws URISyntaxException
-     */
-    @Test
-    void givenRestTemplate_whenRequested_thenLogAndModifyResponse() throws InterruptedException, URISyntaxException {
-
-        assertNotNull(restTemplate);
-        assertNotNull(myCacheOfResponse);
-
-        LoginForm loginForm0 = new LoginForm("userName", "password");
-        LoginForm loginForm1 = new LoginForm("userName2", "password2");
-
-        long initialNumberOfRequests = myCacheOfResponse.getNumberOfExecutedRequests();
-
-        callRestTemplateWithCaching(loginForm0, Cache.NOT_CACHED); // cache miss
-        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
-        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
-        TimeUnit.SECONDS.sleep(MAX_TIME_SPAN_SECONDS + 10); // wait until it is too late for caching
-        callRestTemplateWithCaching(loginForm0, Cache.NOT_CACHED); // cache miss: too long ago
-        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
-        callRestTemplateWithCaching(loginForm1, Cache.NOT_CACHED); // cache miss: different body
-        callRestTemplateWithCaching(loginForm0, Cache.CACHED); // cache hit
-        callRestTemplateWithCaching(loginForm1, Cache.CACHED); // cache hit
-
-        assertEquals(initialNumberOfRequests + 3, myCacheOfResponse.getNumberOfExecutedRequests());
-
-    }
-
-    /**
-     * @param loginForm
-     * @param cacheStatus
-     * @throws URISyntaxException
-     */
-    private void callRestTemplateWithCaching(LoginForm loginForm, Cache cacheStatus) throws URISyntaxException {
-        URI TESTSERVICEURL = new URI("http://httpbin.org/post");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<LoginForm> requestEntity = new HttpEntity<LoginForm>(loginForm, headers);
-
-        long numberOfNewlyExecutedRequests = myCacheOfResponse.getNumberOfExecutedRequests();
-        logger.debug("execute POST request on {}", TESTSERVICEURL);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(TESTSERVICEURL, requestEntity, String.class);
-        numberOfNewlyExecutedRequests = myCacheOfResponse.getNumberOfExecutedRequests() - numberOfNewlyExecutedRequests;
-        logger.info("numberOfExecutedRequest since last request: new={}, count={}, teststatus={}", //
-                numberOfNewlyExecutedRequests, myCacheOfResponse.getNumberOfExecutedRequests(), cacheStatus);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-
-        switch (cacheStatus) {
-            case NOT_CACHED:
-                assertEquals(1, numberOfNewlyExecutedRequests);
-                break;
-            case CACHED:
-                assertEquals(0, numberOfNewlyExecutedRequests);
-                break;
-            default:
-                fail("Test case misconfigured");
-                break;
-        }
-    }
-
-    private enum Cache {
-        CACHED, NOT_CACHED
-    }
-
-    public class LoginForm {
-        private String username;
-        private String password;
-
-        public LoginForm() {
-        }
-
-        public LoginForm(String username, String password) {
-            super();
-            this.username = username;
-            this.password = password;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
     }
 
     @Test
