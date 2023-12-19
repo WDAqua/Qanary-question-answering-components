47f69635bd0203d5bb8b46d1156ebd20d9a8ddf7
anbo-de <github@andreasboth.de>
2023-12-19 03:24:43 +0100

    LD-shuyo: upgrading Spring Boot Starter Parent

--- qanary-component-LD-Shuyo/src/main/java/eu/wdaqua/qanary/component/shuyo/ld/Application.java
+++ qanary-component-LD-Shuyo/src/main/java/eu/wdaqua/qanary/component/shuyo/ld/Application.java
@@ -1,13 +1,41 @@
 package eu.wdaqua.qanary.component.shuyo.ld;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.cybozu.labs.langdetect.LangDetectException;
+
+import eu.wdaqua.qanary.component.QanaryComponent;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"eu.wdaqua.qanary"})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		String appVersion = getClass().getPackage().getImplementationVersion();
+		return new OpenAPI().info(new Info() //
+				.title("Qanary Shuyo Language Detection Component") //
+				.version(appVersion) //
+				.description("LD") //
+				.termsOfService("http://swagger.io/terms/") //
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")) //
+		);
+	}
+	
+	@Bean
+	public QanaryComponent qanaryComponent(@Value("${spring.application.name}") final String name) throws IOException, LangDetectException {
+		return new LanguageDetection(name);
 	}
 }
