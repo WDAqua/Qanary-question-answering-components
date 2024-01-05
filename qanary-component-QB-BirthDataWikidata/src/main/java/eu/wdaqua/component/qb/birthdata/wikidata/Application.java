4a2a2f592e61879566b34b92180d2d43290762ce
anbo-de <github@andreasboth.de>
2024-01-05 02:03:06 +0100

    QB-BirthDataWikidata v3.4.0: ext. functionality
    * separated functionality: 1. use named entities that are linked, 2. use firstname and lastname annotations
    * improved SPARQL queries
    * added additional documentation

--- qanary-component-QB-BirthDataWikidata/src/main/java/eu/wdaqua/component/qb/birthdata/wikidata/Application.java
+++ qanary-component-QB-BirthDataWikidata/src/main/java/eu/wdaqua/component/qb/birthdata/wikidata/Application.java
@@ -14,10 +14,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"eu.wdaqua.qanary"})
+@ComponentScan(basePackages = { "eu.wdaqua.qanary" })
 /**
- * basic class for wrapping functionality to a Qanary component
- * note: there is no need to change something here
+ * basic class for wrapping functionality to a Qanary component note: there is
+ * no need to change something here
  */
 public class Application {
 
@@ -29,8 +29,8 @@ public class Application {
 	}
 
 	/**
-	 * this method is needed to make the QanaryComponent in this project known
-	 * to the QanaryServiceController in the qanary_component-template
+	 * this method is needed to make the QanaryComponent in this project known to
+	 * the QanaryServiceController in the qanary_component-template
 	 *
 	 * @return
 	 */
@@ -38,9 +38,10 @@ public class Application {
 	public QanaryComponent qanaryComponent(@Value("${spring.application.name}") final String applicationName) {
 		return new BirthDataQueryBuilder(applicationName);
 	}
-	
-	@Bean 
-	public BirthDataQueryBuilderController getBirthDataQueryBuilderController(BirthDataQueryBuilder myBirthDataQueryBuilder) {
+
+	@Bean
+	public BirthDataQueryBuilderController getBirthDataQueryBuilderController(
+			BirthDataQueryBuilder myBirthDataQueryBuilder) {
 		return new BirthDataQueryBuilderController(myBirthDataQueryBuilder);
 	}
 
