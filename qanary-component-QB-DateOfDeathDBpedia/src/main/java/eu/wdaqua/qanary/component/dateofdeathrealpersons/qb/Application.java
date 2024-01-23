69578240b6c439c6aad6ceb171a8d9b14a8938a6
heinpa <heipa@posteo.de>
2024-01-23 17:32:11 +0100

    DateOfDeathDBpedia reads appVersion from POM

--- qanary-component-QB-DateOfDeathDBpedia/src/main/java/eu/wdaqua/qanary/component/dateofdeathrealpersons/qb/Application.java
+++ qanary-component-QB-DateOfDeathDBpedia/src/main/java/eu/wdaqua/qanary/component/dateofdeathrealpersons/qb/Application.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import eu.wdaqua.qanary.component.QanaryComponentConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import eu.wdaqua.qanary.component.QanaryComponent;
 
 @SpringBootApplication
@@ -38,4 +41,17 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		String appVersion = getClass().getPackage().getImplementationVersion();
+		return new OpenAPI().info(new Info() //
+				.title("QB DateOfDeathDBpedia component") //
+				.version(appVersion) //
+				.description("This is a sample Foobar server created using springdocs - "
+						+ "a library for OpenAPI 3 with spring boot.")
+				.termsOfService("http://swagger.io/terms/") //
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")) //
+		);
+	}
 }
