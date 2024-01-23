8b434587ab6a87d48b8980bff65ccffd3ad43206
heinpa <heipa@posteo.de>
2024-01-23 17:29:25 +0100

    KG2KG-TranslateAnnotationsOfInstance reads version from POM

--- qanary-component-KG2KG-TranslateAnnotationsOfInstance/src/main/java/eu/wdaqua/qanary/component/Application.java
+++ qanary-component-KG2KG-TranslateAnnotationsOfInstance/src/main/java/eu/wdaqua/qanary/component/Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"eu.wdaqua.qanary"})
 public class Application {
@@ -19,5 +23,18 @@ public class Application {
     public QanaryComponent qanaryComponent(
             @Value("${spring.application.name}") final String applicationName) {
         return new KG2KGTranslateAnnotationsOfInstance(applicationName);
+    } 
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String appVersion = getClass().getPackage().getImplementationVersion();
+        return new OpenAPI().info(new Info() //
+            .title("NED DBpediaSpotlight component") //
+            .version(appVersion) //
+            .description("This is a sample Foobar server created using springdocs - "
+            + "a library for OpenAPI 3 with spring boot.")
+            .termsOfService("http://swagger.io/terms/") //
+            .license(new License().name("Apache 2.0").url("http://springdoc.org")) //
+            );
     }
 }
