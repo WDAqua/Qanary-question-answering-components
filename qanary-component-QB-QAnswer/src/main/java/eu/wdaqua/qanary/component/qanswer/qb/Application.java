3a75664b25961065e5a176337784d163251d695f
heinpa <heipa@posteo.de>
2024-01-23 17:34:38 +0100

    QB-QAnswer reads appVersion from POM

--- qanary-component-QB-QAnswer/src/main/java/eu/wdaqua/qanary/component/qanswer/qb/Application.java
+++ qanary-component-QB-QAnswer/src/main/java/eu/wdaqua/qanary/component/qanswer/qb/Application.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -51,5 +55,18 @@ public class Application {
     @Bean(name = "endpointUrl")
     URI endpointUrl(@Value("${qanswer.endpoint.url}") String endpointUrl) throws URISyntaxException {
         return new URI(endpointUrl);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String appVersion = getClass().getPackage().getImplementationVersion();
+        return new OpenAPI().info(new Info() //
+            .title("NED DBpediaSpotlight component") //
+            .version(appVersion) //
+            .description("This is a sample Foobar server created using springdocs - "
+            + "a library for OpenAPI 3 with spring boot.")
+            .termsOfService("http://swagger.io/terms/") //
+            .license(new License().name("Apache 2.0").url("http://springdoc.org")) //
+        );
     }
 }
